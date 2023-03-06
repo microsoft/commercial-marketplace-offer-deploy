561b78f03c79397b792524933cbf6dddf849cd2a
Bob Jacobs <jacobsrj@hotmail.com>
2023-03-06 13:07:46 -0500

    Feature/deployment dry run (#11)
    * DryRun and accompanying tests
    
    * adding additional unit tests for deployment package
    
    * code cleanup
    
    * code cleanup

---
+++ pkg/deployment/deployment.go
@@ -0,0 +1,179 @@
+package deployment
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
+)
+
+type AzureDeployment struct {
+	subscriptionId string
+	location string
+	resourceGroupName string
+	deploymentName string
+	template map[string]interface{}
+	params map[string]interface{}
+}
+
+func (azureDeployment *AzureDeployment) validate() error {
+	if len(azureDeployment.subscriptionId) == 0 {
+		return errors.New("subscriptionId is not set on azureDeployment input struct")
+	}
+	if len(azureDeployment.location) == 0 {
+		return errors.New("location is not set on azureDeployment input struct")
+	}
+	if len(azureDeployment.resourceGroupName) == 0 {
+		return errors.New("resourceGroupName is not set on azureDeployment input struct")
+	}
+	if len(azureDeployment.resourceGroupName) == 0 {
+		return errors.New("resourceGroupName is not set on azureDeployment input struct")
+	}
+	if azureDeployment.template == nil {
+		return errors.New("template is not set on deployment azureDeployment struct")
+	}
+	// allow params to be empty to support all default params
+	return nil
+}
+
+func DeleteResourceGroup(subscriptionId string, resourceGroupName string) (error) {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	resourceGroupClient, err := armresources.NewResourceGroupsClient(subscriptionId, cred, nil)
+	if err != nil {
+		return err
+	}
+
+	pollerResp, err := resourceGroupClient.BeginDelete(ctx, resourceGroupName, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = pollerResp.PollUntilDone(ctx, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func CreateResourceGroup(subscriptionId string, resourceGroupName string, location string) (*armresources.ResourceGroup, error) {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+
+	resourceGroupClient, err := armresources.NewResourceGroupsClient(subscriptionId, cred, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resourceGroupResp, err := resourceGroupClient.CreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		armresources.ResourceGroup{
+			Location: to.Ptr(location),
+		},
+		nil)
+	if err != nil {
+		return nil, err
+	}
+	return &resourceGroupResp.ResourceGroup, nil
+}
+
+func Create(deploymentName, subscriptionId, resourceGroupName string, template, params map[string]interface{}) (*armresources.DeploymentExtended, error) {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	deploymentsClient, err := armresources.NewDeploymentsClient(subscriptionId, cred, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("About to Create a deployment")
+
+	deploymentPollerResp, err := deploymentsClient.BeginCreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		deploymentName,
+		armresources.Deployment{
+			Properties: &armresources.DeploymentProperties{
+				Template:   template,
+				Parameters: params,
+				Mode:       to.Ptr(armresources.DeploymentModeIncremental),
+			},
+		},
+		nil)
+
+	if err != nil {
+		return nil, fmt.Errorf("cannot create deployment: %v", err)
+	}
+
+	resp, err := deploymentPollerResp.PollUntilDone(ctx, nil)
+	if err != nil {
+		return nil, fmt.Errorf("cannot get the create deployment future respone: %v", err)
+	}
+
+	return &resp.DeploymentExtended, nil
+}
+
+func DryRun(azureDeployment *AzureDeployment) string {
+	err := azureDeployment.validate()
+	if err != nil {
+		log.Fatal(err)
+	}
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+
+	whatIfResult, err := whatIfDeployment(ctx, cred, azureDeployment)
+	if err != nil {
+		log.Fatal(err)
+	}
+	whatifdata, _ := json.Marshal(whatIfResult)
+	return string(whatifdata)
+}
+
+func whatIfDeployment(ctx context.Context, cred azcore.TokenCredential, azureDeployment *AzureDeployment) (*armresources.DeploymentsClientWhatIfResponse, error) {
+	deploymentsClient, err := armresources.NewDeploymentsClient(azureDeployment.subscriptionId, cred, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	pollerResp, err := deploymentsClient.BeginWhatIf(
+		ctx,
+		azureDeployment.resourceGroupName,
+		azureDeployment.deploymentName,
+		armresources.DeploymentWhatIf{
+			Properties: &armresources.DeploymentWhatIfProperties{
+				Template:   azureDeployment.template,
+				Parameters: azureDeployment.params,
+				Mode:       to.Ptr(armresources.DeploymentModeIncremental),
+			},
+		},
+		nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := pollerResp.PollUntilDone(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}
