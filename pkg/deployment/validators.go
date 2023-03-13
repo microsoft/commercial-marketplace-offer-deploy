42c188ed1825467c238a2bc4f8b0f645c7c28f4a
Bob Jacobs <bob.jacobs@outlook.com>
2023-03-13 18:36:08 -0400

    changing input to DryRunValidationInput

--- pkg/deployment/validators.go
+++ pkg/deployment/validators.go
@@ -1,11 +1,22 @@
 package deployment
 
+import (
+	"context"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+)
+
 type DryRunValidator interface {
-	Validate(azureDeployment *AzureDeployment) *DryRunResponse
+	Validate(input DryRunValidationInput) *DryRunResponse
 }
 
-type WhatIfValidatorFunc func(azureDeployment *AzureDeployment) *DryRunResponse
+type WhatIfValidatorFunc func(input DryRunValidationInput) *DryRunResponse
 
-func (f WhatIfValidatorFunc) Validate(azureDeployment *AzureDeployment) *DryRunResponse {
-	return f(azureDeployment)
+func (f WhatIfValidatorFunc) Validate(input DryRunValidationInput) *DryRunResponse {
+	return f(input)
+}
+
+type DryRunValidationInput struct {
+	ctx context.Context
+	cred azcore.TokenCredential
+	azureDeployment *AzureDeployment
 }
