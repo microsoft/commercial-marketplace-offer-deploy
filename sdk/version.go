fb0fc0b5653dbb9517bd7ee3d072b6db4adc4549
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-03-15 16:24:10 -0400

    initial "custom" client

---
+++ sdk/version.go
@@ -0,0 +1,10 @@
+package sdk
+
+// Constants to identify the module
+const (
+	// moduleName is the module name that shows in telemetry.
+	moduleName = "cmodm"
+
+	// moduleVersion is the semantic version (see http://semver.org) of this module.
+	moduleVersion = "v0.1.0"
+)
