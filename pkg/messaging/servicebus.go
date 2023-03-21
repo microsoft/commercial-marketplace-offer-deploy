e09288c7ea62f4a69aa010a95969dd03251d6d49
Bob Jacobs <bob.jacobs@outlook.com>
2023-03-21 12:41:01 -0400

    adding servicebus implementation

--- pkg/messaging/servicebus.go
+++ pkg/messaging/servicebus.go
@@ -2,7 +2,11 @@ package messaging
 
 import (
 	"context"
+	"encoding/json"
+	"os"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
 )
 
@@ -12,20 +16,47 @@ func (f ServiceBusPublisher) Publish(message DeploymentMessage) error {
 	return f(message)
 }
 
-// func NewServiceBusPublisher(connectionString string) (ServiceBusPublisher, error) {
-// 	client, err := azservicebus.NewClientFromConnectionString(connectionString)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+func NewServiceBusPublisher(queueName string) (ServiceBusPublisher, error) {
+	ns := os.Getenv("SERVICEBUS_ENDPOINT")
+	var credsToAdd []azcore.TokenCredential
+	cliCred, err := azidentity.NewAzureCLICredential(nil)
+	if err != nil {
+		return nil, err
+	}
+	envCred, err := azidentity.NewEnvironmentCredential(nil)
+	if err != nil {
+		return nil, err
+	}
 
-// 	return func(message DeploymentMessage) error {
-// 		sender, err := client.NewSender(azservicebus.TopicName(message.Header.Topic))
-// 		if err != nil {
-// 			return err
-// 		}
+	//todo: adjust client credentials in accordance to api
+	credsToAdd = append(credsToAdd, cliCred, envCred)
+	cred, err := azidentity.NewChainedTokenCredential(credsToAdd, nil)
+	if err != nil {
+		return nil, err
+	}
 
-// 		return sender.SendMessage(context.Background(), azservicebus.Message{
-// 			Body: message.Body,
-// 		})
-// 	}, nil
-// }
+	client, err := azservicebus.NewClient(ns, cred, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return func(message DeploymentMessage) error {
+		sender, err := client.NewSender(queueName, nil)
+		if err != nil {
+			return err
+		}
+		defer sender.Close(context.TODO())
+
+		jsonContent, err := json.Marshal(message)
+		if err != nil {
+			return err
+		}
+
+		sbMessage := &azservicebus.Message {
+			Body: []byte(jsonContent),
+		}
+
+		return sender.SendMessage(context.TODO(), sbMessage, nil)
+	}, nil
+}
