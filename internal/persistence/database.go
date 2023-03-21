3ff1f7ba9934f840244e18c9bc715bcb9f660f5d
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-03-21 16:23:10 -0400

    Feature/issue 40 persistence (#44)
    * persistence
    
    * updating configuration to include db information,

---
+++ internal/persistence/database.go
@@ -0,0 +1,88 @@
+package persistence
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Database interface {
+	Instance() *gorm.DB
+}
+
+type DatabaseOptions struct {
+	UseInMemory bool
+	Dsn         string
+}
+
+type database struct {
+	db *gorm.DB
+}
+
+const (
+	DefaultDatabasePath = "/data/cmod/"
+	DatabaseName        = "commercial-marketplace-offer-deploy"
+	DatabaseFileName    = DatabaseName + ".db"
+	InMemoryDsn         = "file::memory:?cache=shared"
+)
+
+// Db implements DbContext
+func (ctx *database) Instance() *gorm.DB {
+	return ctx.db
+}
+
+// Gets a new db using the provided Options. If options are nil, the default DSN is used
+func NewDatabase(options *DatabaseOptions) Database {
+	if options == nil {
+		options = &DatabaseOptions{Dsn: getDefaultDsn()}
+	}
+
+	var db *gorm.DB
+	if options.UseInMemory {
+		db = newInMemoryDatabase()
+	} else {
+		db = newDatabase(options.Dsn)
+	}
+
+	return &database{db}
+}
+
+func newInMemoryDatabase() *gorm.DB {
+	return newDatabase(InMemoryDsn)
+}
+
+// creates a new database, establishing an open connection with a new session
+// path: the folder path to the database file
+func newDatabase(dsn string) *gorm.DB {
+	db, err := createInstance(dsn)
+
+	if err != nil {
+		log.Fatalf("Could not open database %s. Error: %v", dsn, err)
+	}
+
+	return db
+}
+
+func createInstance(dsn string, models ...interface{}) (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return nil, fmt.Errorf("could not open and connect to database at %s: %w", dsn, err)
+	}
+
+	if err := db.AutoMigrate(models); err != nil {
+		return nil, fmt.Errorf("could not migrate models %T: %w", models, err)
+	}
+
+	if tx := db.Exec("PRAGMA foreign_keys = ON", nil); tx.Error != nil {
+		return nil, fmt.Errorf("unable to turn on foreign keys in sqlite db: %w", tx.Error)
+	}
+	return db, nil
+}
+
+func getDefaultDsn() string {
+	return filepath.Join(DefaultDatabasePath, DatabaseFileName)
+}
