6b77897a1e732b950e677d41a52570e368a4b8db
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-03-23 09:56:52 -0400

    updating mapper for create deployment

--- internal/data/mappers.go
+++ internal/data/mappers.go
@@ -8,9 +8,12 @@ func FromCreateDeployment(from *generated.CreateDeployment) *Deployment {
 	//TODO: parse out template into the stages
 
 	deployment := &Deployment{
-		Name:     *from.Name,
-		Status:   "New",
-		Template: from.Template.(map[string]interface{}),
+		Name:           *from.Name,
+		Status:         "New",
+		SubscriptionId: *from.SubscriptionID,
+		ResourceGroup:  *from.ResourceGroup,
+		Location:       *from.Location,
+		Template:       from.Template.(map[string]interface{}),
 	}
 	return deployment
 }
