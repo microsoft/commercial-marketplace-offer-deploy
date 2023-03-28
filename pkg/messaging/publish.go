21c58e67fae3de1834807a8a877464d81587c242
Bob Jacobs <bob.jacobs@outlook.com>
2023-03-28 00:04:10 -0400

    adding integration test

--- pkg/messaging/publish.go
+++ pkg/messaging/publish.go
@@ -1,15 +1,39 @@
 package messaging
 
+import (
+	"errors"
+	"fmt"
+)
+
+
 type Publisher interface {
 	Publish(message DeploymentMessage) error
 }
 
 type DeploymentMessage struct {
 	Header DeploymentMessageHeader  `json:"header"`
-	Body interface{} `json:"body"`
+	Body any `json:"body"`
 }
 
 type DeploymentMessageHeader struct {
 	Topic string `json:"topic"`
+}
+
+type PublisherConfig struct {
+	Type string
+	TypeConfig any
+}
+
+func CreatePublisher(config PublisherConfig) (Publisher, error) {
+	switch config.Type {
+		case "servicebus": {
+			serviceBusConfig := config.TypeConfig.(ServiceBusConfig)
+			return NewServiceBusPublisher(serviceBusConfig.namespace, serviceBusConfig.queueName)
+		}
+		default: {
+			errorMsg := fmt.Sprintf("There was no publisher for the provided type %s", config.Type)
+			return nil, errors.New(errorMsg)
+		}
+	}
 }
 
---
+++ pkg/messaging/servicebus_test.go
@@ -0,0 +1,3 @@
+package messaging_test
+
+
