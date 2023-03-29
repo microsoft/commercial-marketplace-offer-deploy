4dc09a97ee5ddd28fb7d782d0551189f0b00523a
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-03-29 13:45:37 -0400

    adding deployment events client

---
+++ internal/azure/eventgrid/deploymenteventsclient.go
@@ -0,0 +1,107 @@
+package eventgrid
+
+import (
+	"context"
+	"strings"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/eventgrid/armeventgrid"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
+)
+
+type DeploymentEventsClient interface {
+	CreateSystemTopic(ctx context.Context) (*armeventgrid.SystemTopic, error)
+	CreateEventSubscription(ctx context.Context) error
+}
+
+type deploymentEventsClient struct {
+	Credential azcore.TokenCredential
+	Properties *eventGridManagerProperties
+}
+
+// Creates an event grid manager to create system topic and event subscription for the purpose of receiving deployment events
+// It will use the resource group id to create the system topic in the resource group's location and subscription
+func NewDeploymentEventsClient(credential azcore.TokenCredential, resourceGroupId string) (DeploymentEventsClient, error) {
+	properties, err := getProperties(context.TODO(), credential, resourceGroupId)
+
+	if err != nil {
+		return nil, err
+	}
+	client := &deploymentEventsClient{
+		Credential: credential,
+		Properties: properties,
+	}
+	return client, nil
+}
+
+func (c *deploymentEventsClient) CreateEventSubscription(ctx context.Context) error {
+	_, err := armeventgrid.NewEventSubscriptionsClient(c.Properties.SubscriptionId, c.Credential, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *deploymentEventsClient) CreateSystemTopic(ctx context.Context) (*armeventgrid.SystemTopic, error) {
+	systemTopicsClient, err := armeventgrid.NewSystemTopicsClient(c.Properties.SubscriptionId, c.Credential, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	pollerResp, err := systemTopicsClient.BeginCreateOrUpdate(
+		ctx,
+		c.Properties.ResourceGroupId,
+		c.Properties.SystemTopicName,
+		armeventgrid.SystemTopic{
+			Location: c.Properties.Location,
+			Properties: &armeventgrid.SystemTopicProperties{
+				Source:    to.Ptr(c.Properties.ResourceGroupId),
+				TopicType: to.Ptr("Microsoft.Resources.ResourceGroups"),
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := pollerResp.PollUntilDone(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.SystemTopic, nil
+}
+
+type eventGridManagerProperties struct {
+	SubscriptionId    string
+	Location          *string
+	ResourceGroupName string
+	ResourceGroupId   string
+	SystemTopicName   string
+}
+
+func getProperties(ctx context.Context, cred azcore.TokenCredential, resourceGroupId string) (*systemTopicOptions, error) {
+	values := strings.Split(resourceGroupId, "/")
+	props := &eventGridManagerProperties{
+		SubscriptionId:    values[1],
+		ResourceGroupName: values[len(values)-1],
+		ResourceGroupId:   resourceGroupId,
+	}
+
+	resourceGroupClient, err := armresources.NewResourceGroupsClient(props.SubscriptionId, cred, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	resourceGroup, err := resourceGroupClient.Get(ctx, props.ResourceGroupName, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	props.Location = resourceGroup.Location
+	props.SystemTopicName = props.ResourceGroupName + "Events"
+
+	return props, nil
+}
