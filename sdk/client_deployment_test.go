052695773060f4e370a51001d2f5b4bee74f6294
Kevin M. Gates <kevinmgates@users.noreply.github.com>
2023-03-29 08:34:04 -0500

    Added test deployment

--- sdk/client_deployment_test.go
+++ sdk/client_deployment_test.go
@@ -2,16 +2,38 @@ package sdk
 
 import (
 	"context"
+	"encoding/json"
 	"log"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/labstack/echo"
+	"github.com/microsoft/commercial-marketplace-offer-deploy/cmd/apiserver/handlers"
+	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/data"
+	"github.com/microsoft/commercial-marketplace-offer-deploy/pkg/generated"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+)
+
+var (
+	db             = data.NewDatabase(&data.DatabaseOptions{UseInMemory: true}).Instance()
+	deploymentJson = `{
+		"name":"test-deployment", 
+		"subscriptionId":"test-id",
+		"resourceGroup":"test-rg",
+		"location":"testus",
+		"template": {}
+	}`
+	templateParameters map[string]interface{}
 )
 
 const testEndPoint = "http://localhost:8080"
 
 func TestStartDeployment(t *testing.T) {
+	// Setup
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 
 	if err != nil {
@@ -23,9 +45,23 @@ func TestStartDeployment(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, client)
 
-	// TODO: construct the deployment
-	result, err := client.StartDeployment(context.TODO(), 123, nil)
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(deploymentJson))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
 
+	if assert.NoError(t, handlers.CreateDeployment(c, db)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+
+	var results generated.Deployment
+	json.Unmarshal(rec.Body.Bytes(), &results)
+
+	// TODO: properly construct the startdeployment params
+	result, err := client.StartDeployment(context.Background(), *results.ID, templateParameters)
+
+	// Assertions
 	if err != nil {
 		t.Logf("Error: %s", err)
 	}
