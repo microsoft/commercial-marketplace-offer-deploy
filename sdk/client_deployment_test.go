258b256eb545b5527bdfd78999431647b122d27a
Kevin M. Gates <kevinmgates@users.noreply.github.com>
2023-03-30 08:40:42 -0500

    Feature/startDeployment3-30 (#66)
    * Adding setup docs
    
    * Updated with httptest

--- sdk/client_deployment_test.go
+++ sdk/client_deployment_test.go
@@ -2,32 +2,86 @@ package sdk
 
 import (
 	"context"
+	"encoding/json"
+	"io"
 	"log"
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/utils"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-const testEndPoint = "http://localhost:8080"
+var (
+	deploymentJson = `{
+		"name":"test-deployment", 
+		"subscriptionId":"test-id",
+		"resourceGroup":"test-rg",
+		"location":"testus",
+		"template": {}
+	}`
+)
 
 func TestStartDeployment(t *testing.T) {
+
+	// Setup
+	templateParameters := getParameters(t, "../test/testdata/nameviolation/success/")
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		body, _ := io.ReadAll(r.Body)
+		var received = make(map[string]interface{})
+		json.Unmarshal(body, &received)
+
+		if r.Method == "POST" {
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write([]byte(`{"id": "test-id"}`))
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+		//log.Printf("request: %+v", r)
+		assert.Equal(t, "1", strings.Split(r.RequestURI, "/")[2])
+
+		//get something from templateparams and assert that it is in the request body
+		equals := reflect.DeepEqual(templateParameters, received)
+		assert.True(t, equals)
+		//create a utilty function to compare the two maps
+
+	}))
+	defer ts.Close()
+
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 
 	if err != nil {
 		log.Fatalf("Authentication failure: %+v", err)
 	}
 
-	client, err := NewClient(testEndPoint, cred, nil)
+	client, err := NewClient(ts.URL, cred, nil)
 
 	require.NoError(t, err)
 	require.NotNil(t, client)
 
-	// TODO: construct the deployment
-	result, err := client.StartDeployment(context.TODO(), 123, nil)
+	var ctx context.Context = context.Background()
 
+	// TODO: properly construct the startdeployment params
+	// create
+	_, err = client.StartDeployment(ctx, 1, templateParameters)
+
+	// Assertions
 	if err != nil {
 		t.Logf("Error: %s", err)
 	}
-	require.NotNil(t, result)
+}
+
+func getParameters(t *testing.T, path string) map[string]interface{} {
+	paramsPath := filepath.Join(path, "parameters.json")
+	parameters, err := utils.ReadJson(paramsPath)
+	require.NoError(t, err)
+	return parameters
 }
