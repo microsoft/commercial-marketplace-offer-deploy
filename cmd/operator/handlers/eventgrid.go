b025fb66512afd97b049f7dbb1e3fea49e5bc148
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-03-31 15:22:13 -0400

    web hook handler

--- cmd/operator/handlers/eventgrid.go
+++ cmd/operator/handlers/eventgrid.go
@@ -3,14 +3,41 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm/internal/resource"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
+	"github.com/azure/azure-sdk-for-go/services/eventgrid/2018-01-01/eventgrid"
 	"github.com/labstack/echo"
-	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/azure/eventgrid"
+	internal "github.com/microsoft/commercial-marketplace-offer-deploy/internal/azure/eventgrid"
 	"gorm.io/gorm"
 )
 
-// this handler is the webook endpoint for event grid events
-func EventGridWebHook(c echo.Context, db *gorm.DB) error {
-	webhookValidator := eventgrid.NewWebHookValidationEventHandler(c.Bind)
+// this API handler is the webook endpoint that receives event grid events
+func EventGridWebHookHandler(c echo.Context, db *gorm.DB) error {
+	validationResult := eventGridSubscriptionValidation(c, db)
+
+	if validationResult != nil {
+		return validationResult
+	}
+
+	messages := []eventgrid.Event{}
+	err := c.Bind(&messages)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	for _, message := range messages {
+		resourceId, err := getResourceId(c, message)
+
+		if err != nil {
+			continue
+		}
+		resourcesClient, err := armresources.NewClient(resourceId.SubscriptionID, testsuite.cred, testsuite.options)
+	}
+	return nil
+}
+
+func eventGridSubscriptionValidation(c echo.Context, db *gorm.DB) error {
+	webhookValidator := internal.NewWebHookValidationEventHandler(c.Bind)
 	result := webhookValidator.Handle(c.Request())
 
 	if result.Error != nil {
@@ -21,6 +48,27 @@ func EventGridWebHook(c echo.Context, db *gorm.DB) error {
 		return c.JSON(http.StatusOK, &result.Response)
 	}
 
-	//TODO: handle all other event types
 	return nil
 }
+
+func getResourceId(c echo.Context, message eventgrid.Event) (internal.ResourceEventData, error) {
+	data := message.Data.(internal.ResourceEventData)
+
+	messages := []eventgrid.Event{}
+	err := c.Bind(&messages)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	resourceId, err := resource.ParseResourceID(ResourceURI)
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return resourceId, nil
+}
+
+// func getResource(resourceId resource.ResourceID) (resource.ResourceID, error)) {
+// 	resourcesClient, err := armresources.NewClient(testsuite.subscriptionID, testsuite.cred, testsuite.options)
+// }
