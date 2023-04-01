d24de7bf81926b2bff0ac27dc317b4e125a43e0c
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-04-01 16:22:09 -0400

    map event grid events to resources, relay these over to our web hook publisher

--- cmd/operator/handlers/eventgrid.go
+++ cmd/operator/handlers/eventgrid.go
@@ -1,29 +1,25 @@
 package handlers
 
 import (
-	"log"
+	"context"
 	"net/http"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/services/eventgrid/2018-01-01/eventgrid"
+	"github.com/google/uuid"
 	"github.com/labstack/echo"
-	internal "github.com/microsoft/commercial-marketplace-offer-deploy/internal/azure/eventgrid"
-	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/data"
-	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/events"
+	"github.com/microsoft/commercial-marketplace-offer-deploy/cmd/operator/eventgrid/eventsfiltering"
+	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/messaging"
+	"github.com/microsoft/commercial-marketplace-offer-deploy/pkg/events"
 	"gorm.io/gorm"
 )
 
-// defines handler for event grid webhook
-type EventGridWebHook struct {
-	db      *gorm.DB
-	mapper  internal.EventGridEventMapper
-	filter  events.ResourceFilter
-	Handler echo.HandlerFunc
-}
-
-// this API handler is the webook endpoint that receives event grid events
+// HTTP handler is the webook endpoint that receives event grid events
 // the validation middleware will handle validation requests first before this is reached
-func (h *EventGridWebHook) EventGridWebHookHandler(c echo.Context) error {
+func EventGridWebHook(c echo.Context, db *gorm.DB) error {
+	ctx := c.Request().Context()
 	messages := []*eventgrid.Event{}
 	err := c.Bind(&messages)
 
@@ -31,42 +27,52 @@ func (h *EventGridWebHook) EventGridWebHookHandler(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
-	ctx := c.Request().Context()
-	resources := h.mapper.Map(ctx, messages)
-	resources = h.filter.Filter(resources)
-
-	if len(resources) == 0 {
-		return c.String(http.StatusOK, "No resources to process")
-	}
-
-	for _, resource := range resources {
-		id := *resource.ID
-		tag := *resource.Tags["modm.events"]
-		log.Printf("Resource [%s] capture events '%s'", id, tag)
-	}
-
-	return c.String(http.StatusOK, "OK")
-}
-
-// handler constructor for event grid webhook handler
-func NewEventGridWebHook(databaseOptions *data.DatabaseOptions) *EventGridWebHook {
-	d := data.NewDatabase(databaseOptions)
-	tags := map[string]string{"modm.events": "true"}
-
-	webhook := &EventGridWebHook{
-		db:     d.Instance(),
-		mapper: getMapper(),
-		filter: events.NewResourceTagFilter(tags),
-	}
-	webhook.Handler = webhook.EventGridWebHookHandler
-
-	return webhook
-}
-
-func getMapper() internal.EventGridEventMapper {
 	credential, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		return nil
 	}
-	return internal.NewEventGridEventMapper(credential)
+
+	filter := getEventsFilter(credential)
+	filteredEvents := filter.Filter(ctx, messages)
+
+	if len(filteredEvents) == 0 {
+		return c.String(http.StatusOK, "No resources to process")
+	}
+
+	sendToWebHookPublisher(credential, filteredEvents, ctx)
+	return c.String(http.StatusOK, "OK")
+}
+
+// send these events as our webhook events to the events publisher (that sends to the consumers our events to their webhook endpoint)
+func sendToWebHookPublisher(credential *azidentity.DefaultAzureCredential, filteredEvents []*eventgrid.Event, ctx context.Context) {
+	sender := getMessageSender(credential)
+
+	messages := []*events.WebhookEventMessage{}
+
+	for _, event := range filteredEvents {
+		message := &events.WebhookEventMessage{
+			Id:        uuid.New(),
+			EventType: *event.EventType,
+
+			Payload: map[string]any{
+				"ResourceId": *event.Subject,
+			},
+		}
+		messages = append(messages, message)
+	}
+	sender.Send(ctx, messages)
+}
+
+func getMessageSender(credential azcore.TokenCredential) messaging.MessageSender {
+	return messaging.NewMessageSender(credential)
+}
+
+func getEventsFilter(credential azcore.TokenCredential) eventsfiltering.EventGridEventFilter {
+	// TODO: this should probably come from the db
+	filterTags := eventsfiltering.FilterTags{
+		string(eventsfiltering.FilterTagKeyEvents): to.Ptr("true"),
+	}
+	filter := eventsfiltering.NewTagsFilter(filterTags, credential)
+
+	return filter
 }
