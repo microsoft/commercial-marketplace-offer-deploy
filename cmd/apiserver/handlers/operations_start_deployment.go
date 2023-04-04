47c8778375c0f777d6632b2e8537932bbbc7422d
Kevin M. Gates <kevinmgates@users.noreply.github.com>
2023-04-04 08:11:14 -0500

    Feature/StartDeployment4-3-2 (#80)
    * Updates to messaging
    
    * Updates the deployment status in DB to pending

--- cmd/apiserver/handlers/operations_start_deployment.go
+++ cmd/apiserver/handlers/operations_start_deployment.go
@@ -1,65 +1,112 @@
 package handlers
 
 import (
+	"context"
 	"errors"
 	"log"
+	"time"
+
 	//"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/microsoft/commercial-marketplace-offer-deploy/cmd/operator/config"
 	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/data"
+	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/messaging"
+	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/utils"
 	"github.com/microsoft/commercial-marketplace-offer-deploy/pkg/api"
-	"github.com/microsoft/commercial-marketplace-offer-deploy/pkg/messaging"
+
 	"gorm.io/gorm"
 )
 
-func StartDeployment(deploymentId int, operation api.InvokeDeploymentOperation, db *gorm.DB) (interface{}, error) {
-	log.Printf("Inisde CreateDryRun deploymentId: %d", deploymentId)
+// test struct
+type Message struct {
+	DeploymentId int
+	OperationId  string
+}
 
-	//gather data: deploymentId
-	retrieved := &data.Deployment{}
-	db.First(&retrieved, deploymentId)
+func StartDeployment(deploymentId int, operation api.InvokeDeploymentOperation, db *gorm.DB) (interface{}, error) {
+	log.Printf("Inside StartDeployment deploymentId: %d", deploymentId)
+
+	toUpdate := &data.Deployment{}
+	db.First(&toUpdate, deploymentId)
+	db.Model(&toUpdate).Update("status", "Pending") // TODO: update with deployment.Pending
 
 	templateParams := operation.Parameters
 	if templateParams == nil {
 		return nil, errors.New("templateParams were not provided")
 	}
 
-	//do the work to update the database and post a message to the service bus
+	// ctx := context.TODO()
 
-	// TODO: WRAP in transaction
+	// credential, err := azidentity.NewDefaultAzureCredential(nil)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	//// deployment.Pending
-	config := messaging.PublisherConfig{}
-	config.Type = "servicebus"
-	publisher, _ := messaging.CreatePublisher(config)
+	// // Post message to service bus operator queue
+	// message := Message{
+	// 	DeploymentId: deploymentId,
+	// 	OperationId:  *operation.Name,
+	// }
 
-	message := messaging.DeploymentMessage{
-		Header: messaging.DeploymentMessageHeader{
-			Topic: "TestTopic",
-		},
-		Body: "TestContent",
+	// err = enqueueForPublishing(credential, message, ctx)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// formulate the response
+
+	timestamp := time.Now().UTC()
+	status := "OK"
+
+	returnedResult := api.InvokedOperation{
+		ID:         operation.Name,
+		InvokedOn:  &timestamp,
+		Name:       operation.Name,
+		Parameters: templateParams.(map[string]interface{}),
+		Result:     "0",
+		Status:     &status,
 	}
-	err := publisher.Publish(message)
+	return returnedResult, nil
+}
 
-	// Update DB
+func enqueueForPublishing(credential *azidentity.DefaultAzureCredential, message Message, ctx context.Context) error {
+	sender, err := getMessageSender(credential)
+	if err != nil {
+		return err
+	}
+	results, err := sender.Send(ctx, messaging.OperatorQueue, message)
+	if err != nil {
+		return err
+	}
 
-	//End transaction
-
+	if len(results) > 0 {
+		return utils.NewAggregateError(getErrorMessages(results))
+	}
+	return nil
+}
+func getMessageSender(credential azcore.TokenCredential) (messaging.MessageSender, error) {
+	var appSettings config.AppSettings = config.GetAppSettings()
+	sender, err := messaging.NewServiceBusMessageSender(messaging.MessageSenderOptions{
+		SubscriptionId:      appSettings.Azure.SubscriptionId,
+		Location:            appSettings.Azure.Location,
+		ResourceGroupName:   appSettings.Azure.ResourceGroupName,
+		ServiceBusNamespace: appSettings.Azure.ServiceBusNamespace,
+	}, credential)
 	if err != nil {
 		return nil, err
 	}
 
-	// formulate the response
-	//timestamp := time.Now().UTC()
-	//status := "OK"
-
-	// res := deployment.DryRun(&azureDeployment)
-	// uuid := uuid.New().String()
-	// timestamp := time.Now().UTC()
-	// status := "OK"
-	returnedResult := api.InvokedOperation{
-		ID:        nil,
-		InvokedOn: nil,
-		Result:    nil,
-		Status:    nil,
+	return sender, nil
+}
+func getErrorMessages(sendResults []messaging.SendMessageResult) *[]string {
+	errors := []string{}
+	for _, result := range sendResults {
+		if result.Error != nil {
+			errors = append(errors, result.Error.Error())
+		}
 	}
-	return returnedResult, nil
+
+	return &errors
 }
