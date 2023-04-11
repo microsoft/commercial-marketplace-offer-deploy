c94bb4d054d9a83cd5caa6f84f37e4c4dee6107d
Bob Jacobs <bob.jacobs@outlook.com>
2023-04-11 14:19:21 -0400

    handling multiple services on startup

--- scripts/startservices.sh
+++ scripts/startservices.sh
@@ -5,5 +5,15 @@ rm -rf ${AZURE_STORAGE_MOUNT_POINT}
 mkdir ${AZURE_STORAGE_MOUNT_POINT}
 
 blobfuse ${AZURE_STORAGE_MOUNT_POINT} --use-https=true --tmp-path=/tmp/blobfuse/${AZURE_STORAGE_ACCOUNT} --container-name=${AZURE_STORAGE_ACCOUNT_CONTAINER} -o allow_other
- 
-./operator
+
+# Start the api server
+./apiserver & 
+
+# Start the operator server
+./operator &
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
