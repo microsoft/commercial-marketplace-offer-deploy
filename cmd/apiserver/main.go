10d48499766aa21a96973a531fb8af997ed3cc02
Kevin M. Gates <kevinmgates@users.noreply.github.com>
2023-04-18 12:58:28 -0500

    Update main.go

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	apiserver "github.com/microsoft/commercial-marketplace-offer-deploy/cmd/apiserver/app"
 	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/hosting"
+	logger "github.com/microsoft/commercial-marketplace-offer-deploy/internal/log"
 )
 
 var (
