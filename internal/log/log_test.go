7bdf0a43d96985d6b83464a1baada227f6d80afd
Bob Jacobs <bob.jacobs@outlook.com>
2023-04-24 16:13:57 -0400

    feat: log stactrace (#164)
    * fixing event subscription bug with url
    
    * adding SetCaller to logging
    
    * chaning log formatting to JSON to stay consistent with caddy
    
    * adding logging
    
    * adding additional logging
    
    * adding additional logging
    
    * additional logging
    
    * adding more logging
    
    * fixing forced panic
    
    * removing creds
    
    * adding even grid role assignments
    
    * adding stacktrace to logging

---
+++ internal/log/log_test.go
@@ -0,0 +1,95 @@
+package log
+
+import (
+	"bytes"
+	"fmt"
+
+	//"os"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+type StacktraceHook struct {
+	innerHook logrus.Hook
+}
+
+func (h *StacktraceHook) Levels() []logrus.Level {
+    return logrus.AllLevels
+}
+
+func (h *StacktraceHook) Fire(e *logrus.Entry) error {
+    if v, found := e.Data[logrus.ErrorKey]; found {
+        if err, iserr := v.(error); iserr {
+            type stackTracer interface {
+                StackTrace() errors.StackTrace
+            }
+            if st, isst := err.(stackTracer); isst {
+                stack := fmt.Sprintf("%+v", st.StackTrace())
+                e.Data["stacktrace"] = stack
+            }
+        }
+    }
+	h.innerHook.Fire(e)
+    return nil
+}
+
+func TestLogHook(t *testing.T) {
+
+	env := viper.New()
+	env.AddConfigPath("./testdata")
+	env.SetConfigName(".env")
+	env.SetConfigType("env")
+
+	err := env.ReadInConfig()
+	assert.NoError(t, err)
+ 
+
+	loggingConfig := &LoggingConfig{
+		InstrumentationKey: env.GetString("LOGGING_APP_INSIGHTS_KEY"),
+		DefaultLogLevel:              "Info",
+	}
+
+	insightsConfig := &InsightsConfig{
+		Role:               "MODM",
+		Version:            "1.0",
+		InstrumentationKey: loggingConfig.InstrumentationKey,
+	}
+
+	stacktraceHook := &StacktraceHook{
+		innerHook: &LogrusHook{
+			Client: createTelemetryClient(insightsConfig),
+		},
+	}
+
+	var output bytes.Buffer
+	logrus.SetOutput(&output)
+	logrus.SetLevel(logrus.InfoLevel)
+	logrus.SetReportCaller(true)
+	logrus.SetFormatter(&logrus.TextFormatter{DisableQuote: true})
+    logrus.AddHook(stacktraceHook)
+
+    logrus.WithError(errors.New("Foo")).Error("Wrong")
+
+	outputString := output.String()
+	fmt.Println(outputString)
+
+
+	select{}
+}
+
+func TestLogFormatter(t *testing.T) {
+	var output bytes.Buffer
+	logrus.SetOutput(&output)
+	logrus.SetLevel(logrus.InfoLevel)
+	logrus.SetReportCaller(true)
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	logrus.Error(errors.New("This was a great be failure"))
+
+	outputString := output.String()
+	fmt.Println(outputString)
+}
