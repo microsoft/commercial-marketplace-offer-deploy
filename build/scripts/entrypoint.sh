7c83161d1bd158984576e1d5cd2ca61b58181834
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-04-25 17:18:24 -0400

    sleep for 180seconds before starting

--- build/scripts/entrypoint.sh
+++ build/scripts/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "MODM starting in 3 minutes..."
+sleep 180
 
+echo "API Server starting."
 # Start the api server
 /apiserver /dev/fd/1 2>&1 &
 
+echo "Operator starting."
 # Start the operator server
 /operator /dev/fd/1 2>&1 &
 
