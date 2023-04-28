a3a24e2497873b70ea7a42095051c75e87e06fbf
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-04-28 13:36:39 -0400

    fixing run local to catch failures

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -23,16 +23,31 @@ function run_docker() {
     if [ "$arg" = "build" ]  || [ "$arg" = "true" ]; then
       echo "Building modm container image."
       docker build . -t modm:latest -f ./build/package/Dockerfile --quiet
+      docker_build_result=$?
+
+      if [ $docker_build_result -gt 0 ]; then
+        echo "docker image build failed."
+        echo "exiting."
+        exit 1
+      fi
     fi
 
     # start up ngrok and get address
     ngrok http 8080 > /dev/null &
+    ngrok_start_result=$?
     export NGROK_ID=$!
+
+    if [ $ngrok_start_result -gt 0 ]; then
+      echo "NGROK failed to start."
+      echo "exiting."
+      exit 1
+    fi
+
     echo "NGROK started: $NGROK_ID"
     sleep 2
     export PUBLIC_DOMAIN_NAME=$(curl -s localhost:4040/api/tunnels | jq '.tunnels[0].public_url' -r | sed -E 's/^\s*.*:\/\///g')
     echo "NGROK domain:  $PUBLIC_DOMAIN_NAME \n"
-    
+
     docker compose -f ./deployments/docker-compose.standalone.yml up 
 }
 
@@ -49,7 +64,7 @@ function run_docker_cleanup() {
 
 
 process=$1
-echo "Run: $process"
+echo "Target: $process"
 
 case $process in
 
