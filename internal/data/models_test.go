2a0c70f609374a54774000e21391aa27485384ec
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-05-03 14:10:37 -0400

    updating GORM InvokedOperation with BeforeUpdate to ensure any Result set to empty string

--- internal/data/models_test.go
+++ internal/data/models_test.go
@@ -3,8 +3,11 @@ package data
 import (
 	"testing"
 
+	"github.com/google/uuid"
+	"github.com/microsoft/commercial-marketplace-offer-deploy/pkg/operation"
 	log "github.com/sirupsen/logrus"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -26,4 +29,55 @@ func TestDeploymentTemplateMarshaling(t *testing.T) {
 	log.Print(result.Template)
 	require.NotNil(t, result)
 	require.Equal(t, "test", result.Template["mapField"].(map[string]interface{})["field"])
+}
+
+func TestInvokedOperationUpdate(t *testing.T) {
+	params := make(map[string]interface{})
+	params["stagedId"] = uuid.New()
+
+	model := &InvokedOperation{
+		DeploymentId: 1,
+		Parameters:   params,
+		Name:         string(operation.StatusScheduled),
+		Status:       "test",
+		Result:       make(map[string]interface{}),
+	}
+
+	// save
+	db := NewDatabase(&DatabaseOptions{UseInMemory: true}).Instance()
+	db.Save(model)
+
+	// read
+	var result *InvokedOperation
+	db.First(&result, model.ID)
+
+	// update
+	result.Status = "updated"
+	result.Result = nil
+	db.Save(result)
+
+	assert.NotEqualValues(t, uuid.Nil, result.ID)
+}
+
+func TestDeploymentUpdate(t *testing.T) {
+	params := make(map[string]interface{})
+	params["stagedId"] = uuid.New()
+
+	model := &Deployment{
+		Name:     "test",
+		Status:   "test",
+		Template: params,
+	}
+
+	// save
+	db := NewDatabase(&DatabaseOptions{UseInMemory: true}).Instance()
+	db.Save(model)
+
+	// read
+	var result *Deployment
+	db.First(&result, model.ID)
+
+	// update
+	result.Status = "updated"
+	db.Save(result)
 }
