72cca200102ea8df568fdaa39737a41d0c634c4d
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-05-10 07:17:44 -0400

    v0.2.2

--- sdk/version.go
+++ sdk/version.go
@@ -3,8 +3,8 @@ package sdk
 // Constants to identify the module
 const (
 	// moduleName is the module name that shows in telemetry.
-	moduleName = "marketplaceofferdeploy"
+	moduleName = "commercial-marketplace-offer-deploy"
 
 	// moduleVersion is the semantic version (see http://semver.org) of this module.
-	moduleVersion = "v0.1.0"
+	moduleVersion = "v0.2.2"
 )
