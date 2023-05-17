2dc90acb21db3e3d718131fdd80b1caabb164a52
ashsenth <112658900+ashsenth@users.noreply.github.com>
2023-05-17 11:07:14 -0700

    Added get_deployment_test.go (#262)

---
+++ cmd/apiserver/handlers/deployment_get_test.go
@@ -0,0 +1,59 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/data"
+	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/mapper"
+	"github.com/microsoft/commercial-marketplace-offer-deploy/pkg/api"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetDeployment(t *testing.T) {
+	//new in-memory db
+	db := data.NewDatabase(&data.DatabaseOptions{UseInMemory: true}).Instance()
+
+	//Add deployment to db
+	data := &data.Deployment{}
+	db.Save(data)
+
+	//Setup
+	e := echo.New()
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	request.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	recorder := httptest.NewRecorder()
+	c := e.NewContext(request, recorder)
+	c.SetPath("/:deploymentId")
+	c.SetParamNames("deploymentId")
+	c.SetParamValues(strconv.Itoa(1))
+
+	handler := getDeploymentHandler{
+		db:     db,
+		mapper: mapper.NewDeploymentMapper(),
+	}
+
+	//execute handler
+	err := handler.Handle(c)
+	assert.NoError(t, err)
+
+	//check for invalid ID
+	id, err := handler.getDeploymentId(c)
+	if err != nil {
+		t.Error("Invalid ID")
+	}
+
+	//deployment
+	var jsonResponse api.Deployment
+	json.Unmarshal(recorder.Body.Bytes(), &jsonResponse)
+
+	assert.Equal(t, data.Name, *jsonResponse.Name)
+	responseID := uint(*jsonResponse.ID)
+
+	//check if given ID matches db ID
+	assert.EqualValues(t, id, responseID)
+}
