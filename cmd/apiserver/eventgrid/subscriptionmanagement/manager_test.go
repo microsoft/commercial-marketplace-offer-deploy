e254980346687223c323514ec661e80a25b59309
Kevin Hillinger <kevin.hillinger@outlook.com>
2023-05-17 14:07:26 -0400

    fixing topic name (#261)

--- cmd/apiserver/eventgrid/subscriptionmanagement/manager_test.go
+++ cmd/apiserver/eventgrid/subscriptionmanagement/manager_test.go
@@ -1,55 +1,27 @@
-// +build integration
-
 package subscriptionmanagement
 
 import (
-	"context"
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-// TODO: convert these test to integration tests that use https://github.com/ngrok/ngrok-go to tunnel to localhost
-func ToMove_TestCreateEventSubscription(t *testing.T) {
-	cred, err := azidentity.NewDefaultAzureCredential(nil)
-
-	if err != nil {
-		log.Fatalf("Authentication failure: %+v", err)
+func Test_GetSystemTopicName(t *testing.T) {
+	type test struct {
+		input string
+		want  string
 	}
 
-	resourceGroupId := "/subscriptions/31e9f9a0-9fd2-4294-a0a3-0101246d9700/resourceGroups/sandbox1"
-	client, err := NewEventGridManager(cred, resourceGroupId)
+	tests := []test{
+		{input: "Microsoft-test_rg", want: "testrg-event-topic"},
+		{input: "EventGrid-test_rg", want: "testrg-event-topic"},
+		{input: "System-test_rg", want: "testrg-event-topic"},
+		{input: "rg-that-exceeds-the-max-length-of-fifty-characters-long", want: "rg-that-exceeds-the-max-length-of-fift-event-topic"},
+		{input: ",.~`{}|/<>[]rg-with-special-*&^%$#@!_+=.:'\"", want: "rg-with-special-event-topic"},
+	}
 
-	require.NoError(t, err)
-
-	ctx := context.TODO()
-	subscriptionName := "test-subscription"
-	endpointUrl := "https://2e75-172-73-181-161.ngrok.io/eventgrid"
-
-	result, err := client.CreateEventSubscription(ctx, subscriptionName, endpointUrl)
-
-	assert.NotNil(t, result)
-	require.NoError(t, err)
-
-	assert.Equal(t, subscriptionName, *result.Name)
-}
-
-// integration test. Needs to run against a subscription
-func TestDeploymentEventsClient(t *testing.T) {
-	cred, err := azidentity.NewDefaultAzureCredential(nil)
-
-	resourceGroupId := "/subscriptions/31e9f9a0-9fd2-4294-a0a3-0101246d9700/resourceGroups/test"
-	client, err := NewEventGridManager(cred, resourceGroupId)
-
-	require.NoError(t, err)
-
-	result, err := client.CreateSystemTopic(context.TODO())
-	log.Debugf("result:\n %v", result)
-
-	require.NoError(t, err)
-	assert.NotNil(t, result)
-
+	for _, tc := range tests {
+		got := getSystemTopicName(tc.input)
+		assert.Equal(t, tc.want, got)
+	}
 }
