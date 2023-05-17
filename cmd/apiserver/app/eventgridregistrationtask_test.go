807a2ea33eedf515d66f038adb0c1237cdfb295a
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-05-17 14:37:53 -0400

    event grid subscription name fix

--- cmd/apiserver/app/eventgridregistrationtask_test.go
+++ cmd/apiserver/app/eventgridregistrationtask_test.go
@@ -17,3 +17,23 @@ func Test_createOptions_url(t *testing.T) {
 
 	assert.Equal(t, "https://localhost:8080/eventgrid", result.EndpointUrl)
 }
+
+func Test_getSubscriptionName(t *testing.T) {
+	type test struct {
+		input string
+		want  string
+	}
+
+	tests := []test{
+		{input: "Microsoft-test_rg", want: "testrg-events-" + getHostname()},
+		{input: "EventGrid-test_rg", want: "testrg-events-" + getHostname()},
+		{input: "System-test_rg", want: "testrg-events-" + getHostname()},
+		{input: "rg-that-exceeds-the-max-length-of-sixty-four-characters-long", want: "rg-that-exceeds-the-max-events-" + getHostname()},
+		{input: ",.~`{}|/<>[]rg-with-special-*&^%$#@!_+=.:'\"", want: "rg-with-special-events-" + getHostname()},
+	}
+
+	for _, tc := range tests {
+		got := getSubscriptionName(tc.input)
+		assert.Equal(t, tc.want, got)
+	}
+}
