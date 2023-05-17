63090a6c126a09490bff2647513b24434d04f308
Bob Jacobs <bob.jacobs@outlook.com>
2023-05-17 16:16:34 -0400

    removing unneeded functionality

--- cmd/apiserver/handlers/deployment_invokeoperation_create.go
+++ cmd/apiserver/handlers/deployment_invokeoperation_create.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -11,18 +10,14 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/labstack/echo/v4"
 	"github.com/microsoft/commercial-marketplace-offer-deploy/cmd/apiserver/dispatch"
-	"github.com/microsoft/commercial-marketplace-offer-deploy/cmd/apiserver/utils"
 	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/config"
-	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/data"
 	"github.com/microsoft/commercial-marketplace-offer-deploy/pkg/api"
 	"github.com/microsoft/commercial-marketplace-offer-deploy/pkg/operation"
-	"gorm.io/gorm"
 )
 
 const deploymenIdParameterName = "deploymentId"
 
 type invokeDeploymentOperation struct {
-	db     *gorm.DB
 	dispatcher dispatch.OperatorDispatcher
 }
 
@@ -30,14 +25,6 @@ func (h *invokeDeploymentOperation) Handle(c echo.Context) error {
 	deploymentId, request, err := h.getParameters(c)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	}
-
-	isValid, err := h.validateParameters(deploymentId, request.Parameters)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-	}
-	if !isValid {
-		return echo.NewHTTPError(http.StatusInternalServerError, errors.New("invalid parameters"))
 	}
 
 	operationId, err := h.dispatcher.Dispatch(c.Request().Context(), &dispatch.DispatchInvokedOperation{
@@ -62,30 +49,6 @@ func (h *invokeDeploymentOperation) Handle(c echo.Context) error {
 	return c.JSON(http.StatusOK, response)
 }
 
-func (h *invokeDeploymentOperation) validateParameters(deploymentId int, parameters any) (bool, error) {
-	deployment, err := h.getDeployment(deploymentId)
-	if err != nil {
-		return false, err
-	}
-	if mapParams, ok := parameters.(map[string]any); ok {
-		if deployment.Template != nil && deployment.Template["parameters"] != nil {
-			templateParameters := deployment.Template["parameters"].(map[string]any)
-			hasAllKeys := utils.CheckKeys(templateParameters, mapParams)
-			return hasAllKeys, nil
-		}
-	}
-	return false, errors.New("could not propertly validateParamsters of invoked operation against deployment")
-}
-
-func (h *invokeDeploymentOperation) getDeployment(id int) (*data.Deployment, error) {
-	deployment := &data.Deployment{}
-	result := h.db.First(deployment, id)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-	return deployment, nil
-}
-
 func (h *invokeDeploymentOperation) getParameters(c echo.Context) (int, *api.InvokeDeploymentOperationRequest, error) {
 	deploymentId, err := strconv.Atoi(c.Param(deploymenIdParameterName))
 	if err != nil {
@@ -108,10 +71,8 @@ func NewInvokeDeploymentOperationHandler(appConfig *config.AppConfig, credential
 			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 		}
 
-		db := data.NewDatabase(appConfig.GetDatabaseOptions()).Instance()
 		handler := invokeDeploymentOperation{
 			dispatcher: processor,
-			db:         db,
 		}
 		return handler.Handle(c)
 	}
