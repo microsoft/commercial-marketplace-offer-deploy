13bb20f60f125c064c4d26956ad88dbcfddddb20
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-05-19 14:18:05 -0400

    v1.0.3

--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/iancoleman/strcase v0.2.0
 	github.com/labstack/echo/v4 v4.10.2
 	github.com/lestrrat-go/jwx v1.2.25
-	github.com/microsoft/commercial-marketplace-offer-deploy/sdk v1.0.2
+	github.com/microsoft/commercial-marketplace-offer-deploy/sdk v1.0.3
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.9.0
--- scripts/version.sh
+++ scripts/version.sh
@@ -4,36 +4,21 @@ version=$1
 
 echo "versioning: $version"
 
-echo "- root"
-# parent mod
-go mod tidy
-git commit -m "$version"  
-git tag -a $version -m "$version"  
-git push origin main
-git push origin $version
-
 # sdk
 cd ./sdk
 sdk_version=sdk/$version
-go get github.com/microsoft/commercial-marketplace-offer-deploy@none
-go get github.com/microsoft/commercial-marketplace-offer-deploy@$version
 go mod tidy
 
 git add ./go.mod ./go.sum
 git commit -m "$sdk_version"  
-git push origin main -f
+git push origin v1 -f
 
 git tag -a $sdk_version -m "$sdk_version" 
 git push origin $sdk_version
 
-# # tools
-# cd ../tools
-# go mod tidy
 
-# tools_version=tools/$version
-go get github.com/microsoft/commercial-marketplace-offer-deploy@none
+cd ../
 go get github.com/microsoft/commercial-marketplace-offer-deploy/sdk@none
-go get github.com/microsoft/commercial-marketplace-offer-deploy@$version
 go get github.com/microsoft/commercial-marketplace-offer-deploy/sdk@$version
 
 go mod tidy
