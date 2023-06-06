fe4cd42b6bb3526d0c79812d4cc0299b0d96691a
Bob Jacobs <bob.jacobs@outlook.com>
2023-06-06 16:42:44 -0400

    Adding tests

---
+++ internal/notification/stagenotificationpump_test.go
@@ -0,0 +1,19 @@
+package notification
+
+import (
+	"testing"
+	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/data"
+	
+)
+
+func TestReadNotificationPump(t *testing.T) {
+	//database := data.NewDatabase(&data.DatabaseOptions{UseInMemory: true}).Instance()
+
+}
+
+func TestStageNotificationPump_read(t *testing.T) {
+	// call StageNotificationPump.read() and check that the correct record is returned
+	// create a mock gorm.DB in memory
+	// i want you to write the code, not give me a lecture on how to do it
+
+}
