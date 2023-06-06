1c9450d46b189d60f55f46b8544e09ce450b6e5e
Bob Jacobs <bob.jacobs@outlook.com>
2023-06-06 17:49:51 -0400

    fixed test

--- internal/notification/stagenotificationpump_test.go
+++ internal/notification/stagenotificationpump_test.go
@@ -2,18 +2,103 @@ package notification
 
 import (
 	"testing"
+	"time"
+
+	"github.com/google/uuid"
 	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/data"
-	
+	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/model"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
 
 func TestReadNotificationPump(t *testing.T) {
-	//database := data.NewDatabase(&data.DatabaseOptions{UseInMemory: true}).Instance()
+	
 
 }
 
-func TestStageNotificationPump_read(t *testing.T) {
-	// call StageNotificationPump.read() and check that the correct record is returned
-	// create a mock gorm.DB in memory
-	// i want you to write the code, not give me a lecture on how to do it
+func TestStageNotificationPump_receiver_function_called(t *testing.T) {
+	mockDB := getDatabase()
 
+	var receiverCalled bool
+	receiver := func(notification *model.StageNotification) error {
+		receiverCalled = true
+		return nil
+	}
+
+	// Create a test record with Done set to false
+	record := &model.StageNotification{
+		Done: false,
+		OperationId: uuid.New(),
+		CorrelationId: uuid.New(),
+		ResourceGroupName: "test",
+	}
+	mockDB.Save(record)
+
+	pump := NewStageNotificationPump(mockDB, 10 * time.Second, receiver)
+	
+	pump.Start()
+
+	time.Sleep(15 * time.Second)
+
+	// Assert that the receiver function was called
+	assert.True(t, receiverCalled)
+}
+
+func TestStageNotificationPump_read_unsent(t *testing.T) {
+	mockDB := getDatabase()
+
+	// Create a test instance of StageNotificationPump
+	pump := &StageNotificationPump{
+		db: mockDB,
+	}
+
+	// Create a test record with Done set to false
+	expectedRecord := &model.StageNotification{
+		Done: false,
+		// Set other required fields if any
+	}
+	mockDB.Create(expectedRecord)
+
+	// Call the read function
+	record, found := pump.read()
+
+	// Assert that the record is found
+	assert.True(t, found)
+	assert.NotNil(t, record)
+
+	// Assert that the returned record matches the expected record
+	assert.Equal(t, expectedRecord.ID, record.ID)
+	assert.Equal(t, expectedRecord.Done, record.Done)
+
+	// Assert that Done field is false
+	assert.False(t, record.Done)
+}
+
+func TestStageNotificationPump_read_sent(t *testing.T) {
+	mockDB := getDatabase()
+
+	// Create a test instance of StageNotificationPump
+	pump := &StageNotificationPump{
+		db: mockDB,
+	}
+
+	// Create a test record with Done set to true
+	record := &model.StageNotification{
+		Done: true,
+		// Set other required fields if any
+	}
+	mockDB.Create(record)
+
+	// Call the read function
+	result, found := pump.read()
+
+	// Assert that the record is not found
+	assert.False(t, found)
+	assert.Nil(t, result)
+}
+
+
+func getDatabase() *gorm.DB {
+	database := data.NewDatabase(&data.DatabaseOptions{UseInMemory: true}).Instance()
+	return database
 }
