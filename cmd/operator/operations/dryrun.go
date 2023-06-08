324c2ee95d0383ad7fdc5f3a0b9e02e413ca5ee3
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-06-08 08:52:51 -0400

    cleanup

--- cmd/operator/operations/dryrun.go
+++ cmd/operator/operations/dryrun.go
@@ -4,10 +4,8 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/avast/retry-go"
 	"github.com/microsoft/commercial-marketplace-offer-deploy/internal/model/operation"
 	"github.com/microsoft/commercial-marketplace-offer-deploy/pkg/deployment"
-	"github.com/microsoft/commercial-marketplace-offer-deploy/sdk"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -18,67 +16,16 @@ type dryRunOperation struct {
 }
 
 func (exe *dryRunOperation) Do(context *operation.ExecutionContext) error {
-	invokedOperation := context.Operation()
-
-	retries := uint(invokedOperation.Retries)
-
-	exe.log = log.WithFields(log.Fields{
-		"operationId":  invokedOperation.ID,
-		"deploymentId": invokedOperation.DeploymentId,
-		"attempt":      invokedOperation.Attempts,
-	})
-
-	err := retry.Do(func() error {
-		log := exe.log
-
-		err := context.Running()
-
-		if err != nil {
-			return err
-		}
-
-		log.Info("attempting dry run")
-
-		azureDeployment, err := exe.getAzureDeployment(invokedOperation)
-		var result *sdk.DryRunResult
-
-		if err == nil {
-			result, err = exe.dryRun(context.Context(), azureDeployment)
-		}
-
-		if err != nil {
-			log.Errorf("error executing dry run. error: %v", err)
-			context.Error(err)
-			context.SaveChanges() //save changes instead of retry since we're doing it inline with retry.Do
-
-			return &operation.RetriableError{Err: err, RetryAfter: exe.retryDelay}
-		}
-
-		log.WithField("result", result).Debug("Received dry run result")
-
-		if result != nil {
-			context.Value(result)
-
-		} else {
-			exe.log.Warn("Dry run result was nil")
-		}
-
-		context.Success()
-
-		return nil
-	},
-		retry.Attempts(retries),
-		retry.DelayType(func(n uint, err error, config *retry.Config) time.Duration {
-			if retriable, ok := err.(*operation.RetriableError); ok {
-				return retriable.RetryAfter
-			}
-			return retry.BackOffDelay(n, err, config)
-		}),
-	)
-
+	azureDeployment, err := exe.getAzureDeployment(context.Operation())
 	if err != nil {
-		exe.log.Errorf("Attempts to retry exceeded. Error: %v", err)
+		return err
 	}
+
+	result, err := exe.dryRun(context.Context(), azureDeployment)
+	if err != nil {
+		return err
+	}
+	context.Value(result)
 
 	return err
 }
