563cf69e7fd3acf14931788f665b77f3c0059cd6
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-06-12 23:10:09 -0400

    updating version script

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,24 +7,20 @@ echo "versioning: $version"
 # sdk
 cd ./sdk
 sdk_version=sdk/$version
-go mod tidy
-
-git add ./go.mod ./go.sum
-git commit -m "$sdk_version"  
-git push origin v1 -f
 
 git tag -a $sdk_version -m "$sdk_version" 
 git push origin $sdk_version
 
 
 cd ../
-go get github.com/microsoft/commercial-marketplace-offer-deploy/sdk@none
-go get github.com/microsoft/commercial-marketplace-offer-deploy/sdk@$version
 
+# publish the root/parent
+go get github.com/microsoft/commercial-marketplace-offer-deploy/sdk@$version
 go mod tidy
-git add ./go.mod ./go.sum
-git commit -m "updating tools to consume $version"  
+
+git .
+git commit -m "$version"  
 git push origin main -f
 
-# git tag -a $tools_version -m "$tools_version" 
-# git push origin $tools_version
+git tag -a $version
+git push origin $version
