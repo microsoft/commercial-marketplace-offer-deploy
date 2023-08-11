5db55236785921dee6c43eebbba1f9e1e54a793c
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-08-11 13:41:23 -0400

    jenkins bypass setup wizard and default admin user

--- scripts/v2/build-images.sh
+++ scripts/v2/build-images.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-docker build ./src -t jenkins -f ../build/package/Dockerfile.jenkins  
+docker build ./src -t modm -f ../build/package/Dockerfile.modm  
 docker tag modm:latest modmdev.azurecr.io/modm:latest
-
-docker build . -t modm -f ../build/package/Dockerfile.modm
-docker tag jenkins:latest modmdev.azurecr.io/jenkins:latest
-
-
 docker push modmdev.azurecr.io/modm:latest
+
+docker build . -t jenkins -f ./build/package/Dockerfile.jenkins
+docker tag jenkins:latest modmdev.azurecr.io/jenkins:latest
 docker push modmdev.azurecr.io/jenkins:latest
