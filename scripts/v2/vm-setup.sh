4a14153b0f5b10b7017ac7ce7d727937d2763adc
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-08-14 10:12:43 -0400

    updating caddy file and scripts

--- scripts/v2/vm-setup.sh
+++ scripts/v2/vm-setup.sh
@@ -19,9 +19,17 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo apt install git -y
 
+# caddy
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy -y
+
 # clone repository against develop branch
 git clone --depth=1 --branch develop https://github.com/microsoft/commercial-marketplace-offer-deploy.git
 cd commercial-marketplace-offer-deploy
 
 echo SITE_ADDRESS=$SITE_ADDRESS > ./deployments/.env
+cp ./deployments/Caddy.development ./deployments/Caddy
 sudo docker compose -f ./deployments/docker-compose.development.yml -p modm up
