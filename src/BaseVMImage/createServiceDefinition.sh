cc54480f645671062e5aead2eab04dbfb78b6cc1
Bob Jacobs <bob.jacobs@outlook.com>
2023-08-14 21:25:51 -0400

    adding params to createServiceDefinition.sh

--- src/BaseVMImage/createServiceDefinition.sh
+++ src/BaseVMImage/createServiceDefinition.sh
@@ -2,32 +2,47 @@
 
 # az group create --name bobjacPackageStorage --location eastus2 
 
-az storage account create \
-    --name bobjacmodmscsa29  \
-    --resource-group bobjacPackageStorage \
-    --location eastus2  \
-    --sku Standard_LRS \
-    --kind StorageV2
+# Check if all required parameters are provided
+if [ $# -ne 5 ]; then
+  echo "Usage: $0 <storage_account_resource_group> <storage_account_name> <container_name> <managed_app_resource_group> <managed_app_definition_name>"
+  exit 1
+fi
+STORAGE_ACC_RESOURCE_GROUP="$1"
+STORAGE_ACC_NAME="$2"
+STORAGE_CONTAINER_NAME="$3"
+MA_RESOURCE_GROUP="$4"
+MA_DEFINITION_NAME="$5"
+
+az storage account show --name "$STORAGE_ACC_NAME" --resource-group "$STORAGE_ACC_RESOURCE_GROUP" &>/dev/null
+
+if [ $? -eq 0 ]; then
+    echo "Storage account $STORAGE_ACC_NAME exists in resource group $STORAGE_ACC_RESOURCE_GROUP."
+else
+    echo "Storage account $STORAGE_ACC_NAME does not exist in resource group $STORAGE_ACC_RESOURCE_GROUP."
+    echo "Creating storage account $STORAGE_ACC_NAME in resource group $STORAGE_ACC_RESOURCE_GROUP."
+    az storage account create \
+        --name $STORAGE_ACC_NAME  \
+        --resource-group $STORAGE_ACC_RESOURCE_GROUP \
+        --location eastus2  \
+        --sku Standard_LRS \
+        --kind StorageV2
+fi
 
 az storage container create \
-    --account-name bobjacmodmscsa29 \
-    --name appcontainer \
+    --account-name $STORAGE_ACC_NAME \
+    --name $STORAGE_CONTAINER_NAME \
     --auth-mode login \
     --public-access blob
 
 az storage blob upload \
-    --account-name bobjacmodmscsa29 \
-    --container-name appcontainer \
+    --account-name $STORAGE_ACC_NAME \
+    --container-name $STORAGE_CONTAINER_NAME \
     --name "app.zip" \
     --file "./app.zip"
 
-blob=$(az storage blob url --account-name bobjacmodmscsa29 --container-name appcontainer --name app.zip --output tsv)
+blob=$(az storage blob url --account-name $STORAGE_ACC_NAME --container-name $STORAGE_CONTAINER_NAME --name app.zip --output tsv)
 groupid=$(az ad group show --group bobjacmarketplace --query id --output tsv)
 roleid=$(az role definition list --name Owner --query [].name --output tsv)
 
-az group create --name appDefinitionGroup36 --location eastus2
-az managedapp definition create --name "MODM14" --location "eastus2" --resource-group appDefinitionGroup36 --lock-level ReadOnly --display-name "MODM14" --description "MODM14" --authorizations "$groupid:$roleid" --package-file-uri "$blob"
-
-# This permission was needed to allow the service catalog to work with the image
-# az role assignment create --assignee c3551f1c-671e-4495-b9aa-8d4adcd62976 --role acdd72a7-3385-48ef-bd42-f606fba81ae7 --scope /subscriptions/31e9f9a0-9fd2-4294-a0a3-0101246d9700/resourceGroups/modmImage/providers/Microsoft.Compute/images/bobjacnginx2
-
+az group create --name $MA_RESOURCE_GROUP --location eastus2
+az managedapp definition create --name "$MA_DEFINITION_NAME" --location "eastus2" --resource-group $MA_RESOURCE_GROUP --lock-level ReadOnly --display-name "$MA_DEFINITION_NAME" --description "$MA_DEFINITION_NAME" --authorizations "$groupid:$roleid" --package-file-uri "$blob"
