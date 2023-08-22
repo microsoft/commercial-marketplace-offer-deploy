b2d45221afa1883d0aeb87f786f9c4fcae182736
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-08-22 12:09:59 -0400

    moving docker compose up execution to start up

--- build/vm/scriptExtension.sh
+++ build/vm/scriptExtension.sh
@@ -5,6 +5,18 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-ARTIFACTS_LOCATION="$1"
+export ARTIFACTS_LOCATION="$1"
+export VM_FQDN="$2"
 
 echo "Hello, world from script extension!  The _artifactsLocation is $ARTIFACTS_LOCATION" > scriptExtensionOutput.txt
+echo "FQDN = $VM_FQDN"
+
+# next, setup caddy's required .env file. This file is referenced in the docker-compose file for caddy to run properly and compose
+
+# write the fqdn to an env file that will be used by caddy for its environment variables
+# this will allow the site address to be set to the VM's FQDN
+echo SITE_ADDRESS=$VM_FQDN | sudo tee /usr/local/source/build/vm/caddy/.env
+echo ACME_ACCOUNT_EMAIL=nowhere@nowhere.com  | sudo tee --append /usr/local/source/build/vm/caddy/.env
+
+sudo docker compose -f /usr/local/source/build/vm/docker-compose.yml -p modm up --force-recreate
+# sudo docker compose -f /usr/local/source/build/vm/docker-compose.yml -p modm down
--- build/vm/scripts/build.sh
+++ build/vm/scripts/build.sh
@@ -5,13 +5,3 @@ cd /usr/local/source
 
 sudo docker build ./src -t modm -f ./build/container/Dockerfile.modm  
 sudo docker build . -t jenkins -f ./build/container/Dockerfile.jenkins
-
-# next, setup caddy and compose
-
-# write the fqdn to an env file that will be used by caddy for its environment variables
-# this will allow the site address to be set to the VM's FQDN
-echo SITE_ADDRESS=$VM_FQDN | sudo tee /usr/local/source/build/vm/caddy/.env
-echo ACME_ACCOUNT_EMAIL=nowhere@nowhere.com  | sudo tee --append /usr/local/source/build/vm/caddy/.env
-
-sudo docker compose -f /usr/local/source/build/vm/docker-compose.yml -p modm up --force-recreate
-# sudo docker compose -f /usr/local/source/build/vm/docker-compose.yml -p modm down
