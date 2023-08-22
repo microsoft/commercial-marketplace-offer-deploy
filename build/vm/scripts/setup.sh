800346d7a3d821ba14cdaad69eac45b3f411bb29
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-08-22 11:10:31 -0400

    removing caddy install

--- build/vm/scripts/setup.sh
+++ build/vm/scripts/setup.sh
@@ -20,10 +20,4 @@ echo \
 sudo apt-get -qq update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-# caddy
-sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --yes --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-sudo apt update
-sudo apt install caddy
 
