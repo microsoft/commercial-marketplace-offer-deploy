3cdb850fffd7e28c221776e1c4deaf4db782f4bd
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-08-22 11:52:14 -0400

    updating build script

--- build/vm/scripts/build.sh
+++ build/vm/scripts/build.sh
@@ -12,6 +12,5 @@ sudo docker build . -t jenkins -f ./build/container/Dockerfile.jenkins
 # this will allow the site address to be set to the VM's FQDN
 echo VM_FQDN=$VM_FQDN | sudo tee /usr/local/source/build/vm/caddy/.env
 
-sudo docker compose -f /usr/local/source/build/vm/docker-compose.yml
 sudo docker compose -f /usr/local/source/build/vm/docker-compose.yml -p modm up --force-recreate
 # sudo docker compose -f /usr/local/source/build/vm/docker-compose.yml -p modm down
