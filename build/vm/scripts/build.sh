1fb06ad838152e36263ea39d7ad71fd3b2b36070
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-08-22 11:51:05 -0400

    compose setup

--- build/vm/scripts/build.sh
+++ build/vm/scripts/build.sh
@@ -6,7 +6,12 @@ cd /usr/local/source
 sudo docker build ./src -t modm -f ./build/container/Dockerfile.modm  
 sudo docker build . -t jenkins -f ./build/container/Dockerfile.jenkins
 
-# next, setup caddy
-SITE_ADDRESS=""
-sudo docker compose -f /usr/local/source/build/vm/docker-compose.yml -p modm up -d --force-recreate
+# next, setup caddy and compose
+
+# write the fqdn to an env file that will be used by caddy for its environment variables
+# this will allow the site address to be set to the VM's FQDN
+echo VM_FQDN=$VM_FQDN | sudo tee /usr/local/source/build/vm/caddy/.env
+
+sudo docker compose -f /usr/local/source/build/vm/docker-compose.yml
+sudo docker compose -f /usr/local/source/build/vm/docker-compose.yml -p modm up --force-recreate
 # sudo docker compose -f /usr/local/source/build/vm/docker-compose.yml -p modm down
