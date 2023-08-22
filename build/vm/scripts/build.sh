6d27c89f2944e84ee56b7e8e3b491a0d6b23c3cb
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-08-22 11:59:43 -0400

    updating again

--- build/vm/scripts/build.sh
+++ build/vm/scripts/build.sh
@@ -10,7 +10,8 @@ sudo docker build . -t jenkins -f ./build/container/Dockerfile.jenkins
 
 # write the fqdn to an env file that will be used by caddy for its environment variables
 # this will allow the site address to be set to the VM's FQDN
-echo VM_FQDN=$VM_FQDN | sudo tee /usr/local/source/build/vm/caddy/.env
+echo SITE_ADDRESS=$VM_FQDN | sudo tee /usr/local/source/build/vm/caddy/.env
+echo ACME_ACCOUNT_EMAIL=nowhere@nowhere.com  | sudo tee --append /usr/local/source/build/vm/caddy/.env
 
 sudo docker compose -f /usr/local/source/build/vm/docker-compose.yml -p modm up --force-recreate
 # sudo docker compose -f /usr/local/source/build/vm/docker-compose.yml -p modm down
