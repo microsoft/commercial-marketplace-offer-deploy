305e2c1c1037532da3b8f672c53cb20e68068738
Bob Jacobs <bob.jacobs@outlook.com>
2023-08-22 23:49:11 -0400

    creating test terraform content for script ext

--- build/vm/createAppZip.sh
+++ build/vm/createAppZip.sh
@@ -3,4 +3,8 @@
 package_file="app.zip"  # Replace with the actual file name
 package_file_path=../../bin/$package_file
 
-zip -FS -r -j $package_file_path "../../obj/mainTemplate.json" "../../obj/createUiDefinition.json"
+# Add the terraformcontent directory and its contents to the app.zip
+zip -FS -r -j $package_file_path \
+  "../../obj/mainTemplate.json" \
+  "../../obj/createUiDefinition.json" \
+  "terraformcontent/"
---
+++ build/vm/terraformContent/storage_account.tf
@@ -0,0 +1,20 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "mystorageaccount"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "dev"
+  }
+}
