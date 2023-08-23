34ee4c5147757a7eaa2ffe66b120af7c47632c68
Bob Jacobs <bob.jacobs@outlook.com>
2023-08-22 20:03:08 -0400

    fixing setup script for docker

--- build/vm/scripts/setup.sh
+++ build/vm/scripts/setup.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-
 # prep with prerequisites
-
 sudo apt-get update
 sudo apt-get upgrade -y
 
 # Docker engine
-sudo apt-get updates
+sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg -y
 
-sudo install -m 0755 -d /etc/apt/keyringscurl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg 
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
+# Add Docker GPG key and repository
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker-archive-keyring.gpg
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get -qq update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# Update package lists after adding the repository
+sudo apt-get update
 
+# Install Docker packages
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
+# Optional: Install additional Docker-related packages
+sudo apt-get install docker-buildx-plugin docker-compose -y
