f8ab2aba198e017b7327b024a66a5f457c342351
Bob Jacobs <bob.jacobs@outlook.com>
2023-08-25 11:46:49 -0400

    copy the enture app dir from container to host

--- build/vm/scripts/build.sh
+++ build/vm/scripts/build.sh
@@ -8,6 +8,5 @@ sudo docker build . -t jenkins -f ./build/container/Dockerfile.jenkins
 sudo docker build . --no-cache -t entrypoint-builder -f ./build/container/Dockerfile.modmentry
 
 sudo docker create --name entrypoint-container entrypoint-builder
-sudo docker cp entrypoint-container:/app/Entrypoint /usr/local/bin
-sudo docker cp entrypoint-container:/app/Entrypoint.dll /usr/local/bin
+sudo docker cp entrypoint-container:/app/. /usr/local/bin/
 sudo docker rm entrypoint-container
