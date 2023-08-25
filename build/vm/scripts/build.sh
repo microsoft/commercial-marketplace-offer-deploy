6706ba0cfbc95d78a6e6fa36b7acc97d9fcf8eff
Bob Jacobs <bob.jacobs@outlook.com>
2023-08-25 01:15:52 -0400

    adding Dockerfile.modementry and updating build.sh

--- build/vm/scripts/build.sh
+++ build/vm/scripts/build.sh
@@ -5,3 +5,8 @@ cd /usr/local/source
 
 sudo docker build ./src -t modm -f ./build/container/Dockerfile.modm  
 sudo docker build . -t jenkins -f ./build/container/Dockerfile.jenkins
+sudo docker build --no-cache -t entrypoint-builder -f ./build/container/Dockerfile.modmentry
+
+sudo docker create --name entrypoint-container entrypoint-builder
+sudo docker cp entrypoint-container:/app/Entrypoint /usr/local/bin
+sudo docker rm entrypoint-container
