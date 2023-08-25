3ec141b5512da8ea7ff3da1d8babe9c5db21d42e
Bob Jacobs <bob.jacobs@outlook.com>
2023-08-25 10:34:59 -0400

    adding tearDown.sh script

---
+++ build/vm/scripts/tearDown.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if all required parameters are provided
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <start_version> <end_version>"
+  exit 1
+fi
+
+# Set the range of numbers
+start_number=$1
+end_number=$2
+
+# Loop through the range and construct resource group names
+for ((number=$start_number; number<=$end_number; number++))
+do
+    resource_group_name="modm$number"
+
+    # Get a list of associated resource group names that match the pattern
+    associated_resource_group_names=$(az group list --query "[?name | starts_with(@, 'rg-modm$number')].name" --output tsv)
+
+    # Loop through the associated resource group names and delete them
+    for associated_rg_name in $associated_resource_group_names
+    do
+        echo "Deleting associated resource group: $associated_rg_name"
+        az group delete --name "$associated_rg_name" --yes --no-wait
+    done
+
+    # Delete the main resource group
+    echo "Deleting resource group: $resource_group_name"
+    az group delete --name "$resource_group_name" --yes --no-wait
+done
