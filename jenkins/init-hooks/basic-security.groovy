70eddbc73b9c88e6aedc12e3ebf690eba9336143
Bob Jacobs <bob.jacobs@outlook.com>
2023-09-05 16:50:54 -0400

    fixing groovy script

--- jenkins/init-hooks/basic-security.groovy
+++ jenkins/init-hooks/basic-security.groovy
@@ -5,6 +5,15 @@ import hudson.security.*
 import hudson.util.*;
 import jenkins.install.*;
 import jenkins.model.Jenkins
+import java.io.File
+import hudson.model.FreeStyleProject
+import com.cloudbees.hudson.plugins.folder.*
+
+def jenkinsHome = System.getenv('JENKINS_HOME')
+def jobConfigXmlPath = '/solutions/job-definitions/terraform.xml'
+def jobName = 'modmserviceprincipal'
+def cliJarPath = "${jenkinsHome}/jenkins-cli.jar" // Define the path to jenkins-cli.jar
+def folderName = "${jenkinsHome}/jobs/${jobName}"
 
 println "--> creating local user 'admin'"
 
@@ -22,5 +31,22 @@ strategy.setAllowAnonymousRead(false)
 instance.setAuthorizationStrategy(strategy)
 
 instance.setCrumbIssuer(hudson.security.csrf.GlobalCrumbIssuerConfiguration.createDefaultCrumbIssuer());
+
+def folder = instance.getItem(jobName)
+// Create the folder if it doesn't exist
+if (folder == null) {
+  folder = instance.createProject(Folder.class, jobName)
+}
+
+def xmlContent = new File(jobConfigXmlPath).text
+def xmlStream = new StringBufferInputStream(xmlContent)
+// Check if the job already exists
+def job = folder.getItem(jobName)
+// Remove it if it already exists
+if (job != null) {
+  folder.remove(job)
+}
+// Create job in the folder
+job = folder.createProjectFromXML(jobName, xmlStream)
 
 instance.save()
