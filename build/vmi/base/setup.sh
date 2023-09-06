6bdf25e691490e4b059643be00c2fc2cdc5b20e9
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-09-06 15:40:39 -0400

    fixing warnings in base setup.sh provisioning script

--- build/vmi/base/setup.sh
+++ build/vmi/base/setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # prep with prerequisites
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get upgrade -y
 
 # Docker engine
-sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg -y
 
 # Add Docker GPG key and repository
@@ -17,7 +16,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update package lists after adding the repository
-sudo apt-get update
+sudo apt-get update -y
 
 # Install Docker packages
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
@@ -28,13 +27,11 @@ sudo apt-get install docker-buildx-plugin docker-compose -y
 # Install .NET 7
 sudo apt-get install -y dotnet-sdk-7.0
 
-# Initial image setup
-cd /usr/local/source
+# clone the MODM source into source
+sudo git clone --depth=1 https://github.com/microsoft/commercial-marketplace-offer-deploy.git $MODM_HOME/source
+
+# Initial image setup so we can get cached image layers to speed up builds for the final vmi
+cd $MODM_HOME/source
 
 sudo docker build ./src -t modm -f ./build/container/Dockerfile.modm  
 sudo docker build . -t jenkins -f ./build/container/Dockerfile.jenkins
-sudo docker build . -t entrypoint-builder -f ./build/container/Dockerfile.modmentry
-
-sudo docker create --name entrypoint-container entrypoint-builder
-sudo docker cp entrypoint-container:/app/. /usr/local/bin/
-sudo docker rm entrypoint-container
