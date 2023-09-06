c66aca4a490fdbab7b80a19da07ff3371e8d59a7
Bob Jacobs <bob.jacobs@outlook.com>
2023-09-06 16:14:30 -0400

    deleting dockerComposeTerraform.sh

--- dockerComposeTerraform.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo docker build -t bobjac/jenkins -f ./build/container/Dockerfile.jenkins .
-docker-compose -f docker-compose-terraform.yml up     
-
-
-java -jar ./jenkins-cli.jar -s http://localhost:8083 create-job modmserviceprincipal3 < ./jenkins/definitions/terraform.xml
-
-java -jar ./jenkins-cli.jar -s http://localhost:8083 build modmserviceprincipal3
