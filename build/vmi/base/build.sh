8534a47042805e258dc042498ee8f0da4119f79e
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-09-07 09:03:09 -0400

    updating build script

--- build/vmi/base/build.sh
+++ build/vmi/base/build.sh
@@ -1,14 +1,46 @@
 #!/bin/bash
 
-# import functions
-source ./build/vmi/scripts/shared.sh
+# ===========================================================================================
+#
+#   DESCRIPTION:
+#   This script drives the packer builds
+#   
+#   imports
+    source ./build/vmi/scripts/shared.sh
+# ===========================================================================================
 
-# -------------------------------------------------------------------------------
+# args
+# ------------------------------------------------------------
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+case $1 in
+    -v|--image-version)
+    IMAGE_VERSION="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -*|--*)
+    echo "Unknown option $1"
+    exit 1
+    ;;
+    *)
+    POSITIONAL_ARGS+=("$1") # save positional arg
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+
+guard_against_empty --value "$IMAGE_VERSION" --error-message "Image Version is required. Use --image-version|-v to set."
 
 # Defaults
+# ------------------------------------------------------------
+
 RESOURCE_GROUP_NAME="modm-dev-vmi"
 IMAGE_NAME="modm-base"
-IMAGE_VERSION="0.0.0"
 IMAGE_OFFER="modm-base-ubuntu"
 BUILD_ENVIRONMENT=$(get_build_environment)
 
@@ -22,16 +54,12 @@ if [ -n "$PKR_VAR_resource_group" ]; then
     RESOURCE_GROUP_NAME=$PKR_VAR_resource_group
 fi
 
-source ./build/vmi/scripts/nextversion.sh
-
-if [ $# -ne 1 ]; then
-    IMAGE_VERSION=$(get_next_image_version $IMAGE_NAME $RESOURCE_GROUP_NAME)
-else
-    IMAGE_VERSION="$1"
-fi
 
 export PKR_VAR_image_name="${IMAGE_NAME}"
 export PKR_VAR_image_version="${IMAGE_VERSION}"
+
+# DEMANDS
+demand_resource_group $RESOURCE_GROUP_NAME
 
 # PREAMBLE
 echo ""
@@ -46,51 +74,13 @@ echo ""
 echo "Packer Variables "
 echo "---------------------------------------------"
 print_packer_variables
-
-
-# Check if the Shared Image Gallery exists
-if ! az sig show --resource-group $RESOURCE_GROUP_NAME --gallery-name $PKR_VAR_image_gallery_name &>/dev/null; then
-
-    # Create a Shared Image Gallery
-    az sig create --resource-group $RESOURCE_GROUP_NAME --gallery-name $PKR_VAR_image_gallery_name --location $PKR_VAR_location
-
-    if [ $? -ne 0 ]; then
-        echo "Shared Image Gallery creation failed."
-        exit 1
-    fi
-fi
-
-az sig image-definition show \
-  --subscription $PKR_VAR_subscription_id \
-  --resource-group $RESOURCE_GROUP_NAME \
-  --gallery-name $PKR_VAR_image_gallery_name \
-  --gallery-image-definition $PKR_VAR_image_name \
-  --output none
-
-if [ $? -ne 0 ]; then
-  echo "Creating image definition [$PKR_VAR_image_name] in $PKR_VAR_image_gallery_name"
-  
-  az sig image-definition create \
-    --subscription $PKR_VAR_subscription_id \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --gallery-name $PKR_VAR_image_gallery_name \
-    --gallery-image-definition $PKR_VAR_image_name \
-    --publisher Microsoft \
-    --offer $IMAGE_OFFER \
-    --sku $IMAGE_OFFER \
-    --os-type Linux \
-    --location $PKR_VAR_location
-
-    if [ $? -ne 0 ]; then
-        echo "Image definition creation failed."
-        exit 1
-    fi
-fi
-
 echo ""
-# Run the Packer command
-packer_file=./build/vmi/base/vmi.pkr.hcl
-echo "Executing Packer build against [$packer_file]"
 
-# packer init $packer_file
-# packer build $packer_file
+ensure_shared_image_gallery -g $RESOURCE_GROUP_NAME -n $PKR_VAR_image_gallery_name -l $PKR_VAR_location
+
+ensure_image_definition -s $PKR_VAR_subscription_id -g $RESOURCE_GROUP_NAME -l $PKR_VAR_location \
+    --image-name $PKR_VAR_image_name \
+    --image-gallery-name $PKR_VAR_image_gallery_name \
+    --image-offer $IMAGE_OFFER
+
+execute_packer --image-name $IMAGE_NAME
--- build/vmi/scripts/shared.sh
+++ build/vmi/scripts/shared.sh
@@ -34,7 +34,7 @@ function get_build_environment() {
 }
 
 function export_packer_env_vars_from_file() {
-      # make sure we have a vars file before proceeding
+    # make sure we have a vars file before proceeding
     env_pkrvars_file=./obj/.env.pkrvars
 
     if [ ! -f $env_pkrvars_file ];
@@ -49,6 +49,214 @@ function export_packer_env_vars_from_file() {
     export $(grep -v '^#' $env_pkrvars_file | xargs)
 }
 
+function ensure_shared_image_gallery() {
+    # parse options as --option optionValue
+    POSITIONAL_ARGS=()
+
+    while [[ $# -gt 0 ]]; do
+    case $1 in
+        -n|--gallery-name)
+        name="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -g|--resource-group)
+        rg="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -l|--location)
+        location="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -*|--*)
+        echo "Unknown option $1"
+        exit 1
+        ;;
+        *)
+        POSITIONAL_ARGS+=("$1") # save positional arg
+        shift # past argument
+        ;;
+    esac
+    done
+    set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+    echo "Confirming shared image gallery [$name] exists."
+    az sig show --resource-group $rg --gallery-name $name --query id --output tsv
+
+    # $? will contain the exit status of the last command
+    if [ $? -ne 0 ]; then
+        echo "Creating gallery [$name] in [$rg]."
+        az sig create --resource-group $rg --gallery-name $name --location $location
+    fi
+
+    echo ""
+}
+
+function ensure_image_definition() {
+    # parse options as --option optionValue
+    POSITIONAL_ARGS=()
+
+    while [[ $# -gt 0 ]]; do
+    case $1 in
+        -s|--subscription)
+        subscription_id="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -g|--resource-group)
+        resource_group="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -i|--image-gallery-name)
+        image_gallery_name="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -o|--image-offer)
+        image_offer="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -n|--image-name)
+        image_name="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -l|--location)
+        location="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -*|--*)
+        echo "Unknown option $1"
+        exit 1
+        ;;
+        *)
+        POSITIONAL_ARGS+=("$1") # save positional arg
+        shift # past argument
+        ;;
+    esac
+    done
+    set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+    echo "Confirming image definition [$image_name] exists in the gallery."
+
+    # ensure image definition
+    az sig image-definition show \
+        --subscription $subscription_id \
+        --resource-group $resource_group \
+        --gallery-name $image_gallery_name \
+        --gallery-image-definition $image_name \
+        --output none
+
+    if [ $? -ne 0 ]; then
+    echo "Creating image definition [$image_name] in $image_gallery_name"
+    
+    az sig image-definition create \
+        --subscription $subscription_id \
+        --resource-group $resource_group \
+        --gallery-name $image_gallery_name \
+        --gallery-image-definition $image_name \
+        --publisher Microsoft \
+        --offer $image_offer \
+        --sku $image_offer \
+        --os-type Linux \
+        --location $location
+
+        if [ $? -ne 0 ]; then
+            echo "Image definition creation failed."
+            exit 1
+        fi
+    fi
+}
+
+# resource group should exist. do not automatically create it
+function demand_resource_group() {
+  resource_group=$1
+  echo "Checking Resource Group."
+  az group show -n $resource_group -o none
+  result=$?
+  echo ""
+
+  if [ $result -ne 0 ]; then
+      echo "ERROR: Resource Group [$resource_group] does not exist. aborting vmi build."
+      exit 1
+  fi
+}
+
+function guard_against_empty() {
+    # parse options as --option optionValue
+    POSITIONAL_ARGS=()
+
+    while [[ $# -gt 0 ]]; do
+    case $1 in
+        -v|--value)
+        value="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -m|--error-message)
+        message="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -*|--*)
+        echo "Unknown option $1"
+        exit 1
+        ;;
+        *)
+        POSITIONAL_ARGS+=("$1") # save positional arg
+        shift # past argument
+        ;;
+    esac
+    done
+    set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+    if [ -z "$value" ]; then
+        echo "$message"
+        exit 1
+    fi
+}
+
+function execute_packer() {
+    # parse options as --option optionValue
+    POSITIONAL_ARGS=()
+
+    while [[ $# -gt 0 ]]; do
+    case $1 in
+        -n|--image-name)
+        name="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -*|--*)
+        echo "Unknown option $1"
+        exit 1
+        ;;
+        *)
+        POSITIONAL_ARGS+=("$1") # save positional arg
+        shift # past argument
+        ;;
+    esac
+    done
+    set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+    # Run the Packer command
+    packer_file="./build/vmi/${name}/vmi.pkr.hcl"
+    echo "Executing Packer build [$packer_file]"
+
+    packer init $packer_file
+    packer build $packer_file
+}
+
 export -f pad_string
 export -f print_packer_variables
 export -f get_build_environment
+export -f demand_resource_group
+export -f ensure_shared_image_gallery
+export -f ensure_image_definition
+export -f execute_packer
+export -f guard_against_empty
