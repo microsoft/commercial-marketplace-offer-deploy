3a70f3e4d6496ca141292f6af5ebc78552f50091
Bob Jacobs <bob.jacobs@outlook.com>
2023-09-10 14:35:56 -0400

    fixing terraform templates

--- test/v2/terraform-storage/main.tf
+++ test/v2/terraform-storage/main.tf
@@ -1,10 +1,21 @@
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+data "azurerm_resource_group" "rg" {
+  name = "bobjacterraform"
+}
+
+data "azurerm_resource_group" "example" {
+  name = "bobjacterraform2"
 }
 
 module "storage" {
-  source = "./child-terraform"
-
-  location = "East US"
-  resource_group_name = "bobjacterraform"
+  source               = "./child-terraform"
+  location             = "East US"
+  resource_group_name  = data.azurerm_resource_group.rg.name
 }
--- test/v2/terraform-storage/storage.tf
+++ test/v2/terraform-storage/storage.tf
@@ -1,12 +1,6 @@
-resource "azurerm_resource_group" "example" {
-  name     = "bobjacterraform2"
-  location = "East US"
-}
 
-resource "azurerm_storage_account" "example" {
+
+data "azurerm_storage_account" "example" {
   name                     = "bobjacterra3"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  resource_group_name      = data.azurerm_resource_group.example.name 
 }
