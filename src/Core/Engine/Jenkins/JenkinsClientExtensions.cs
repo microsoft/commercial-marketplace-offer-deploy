ac3e1ca2a134645051aa2209ad9933fcd8747f38
Bob Jacobs <bob.jacobs@outlook.com>
2023-09-11 08:58:50 -0400

    adding status endpoint (#395)

---
+++ src/Core/Engine/Jenkins/JenkinsClientExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using JenkinsNET;
+using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.Extensions.Options;
+
+namespace Modm.Engine.Jenkins
+{
+    public static class JenkinsClientExtensions
+    {
+        public static async Task<EngineStatus> GetJenkinsStatusAsync(this IJenkinsClient client)
+        {
+            EngineStatus status = new EngineStatus { EngineType = EngineType.Jenkins, IsHealthy = false, Version = "Unknown"};
+
+            try
+            {
+                var jenkinsClient = client as JenkinsClient;
+                if (jenkinsClient == null)
+                {
+                    return status;
+                }
+
+                var plainTextBytes = System.Text.Encoding.UTF8.GetBytes($"{jenkinsClient.UserName}:{jenkinsClient.Password}");
+                var credential = Convert.ToBase64String(plainTextBytes);
+
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    // Add Authorization header with the API token
+                    httpClient.DefaultRequestHeaders.Authorization =
+                        new AuthenticationHeaderValue(
+                            "Basic",
+                            credential);
+
+                    HttpResponseMessage response = await httpClient
+                        .GetAsync(jenkinsClient.BaseUrl);
+
+                    response.EnsureSuccessStatusCode();
+                    status.IsHealthy = true;
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    string xJenkinsHeader = response.Headers.GetValues("X-Jenkins").FirstOrDefault();
+                    status.Version = !string.IsNullOrEmpty(xJenkinsHeader) ? xJenkinsHeader : status.Version;
+                    return status;
+                }
+            }
+            catch (Exception ex)
+            {
+                return status;
+            }
+        }
+    }
+}
+
---
+++ src/WebHost/Status/StatusController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using JenkinsNET;
+using Modm.Engine.Jenkins;
+using Modm.Engine;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebHost.Status
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        private readonly IDeploymentEngine engine;
+
+        public StatusController(IDeploymentEngine engine)
+        {
+            this.engine = engine;
+        }
+
+        [HttpGet]
+        public async Task<EngineStatus> GetAsync()
+        {
+            return await this.engine.GetStatus();
+        }
+    }
+}
+
