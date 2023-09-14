dccf9ce07d04b1fdf92c68e4fcbff4297ead395d
Bob Jacobs <bob.jacobs@outlook.com>
2023-09-14 12:17:31 -0400

    Bobjac/scci (#421)
    * setting permission on buil-ma.sh
    
    * adding script resources
    
    * changing directories in scripts
    
    * modifying scripts
    
    * fixing version numbers
    
    * modifying permissions
    
    * fixing image number
    
    * script fixes
    
    * adding echo
    
    * fixing dir paths
    
    * copying content.zip
    
    * fixing path
    
    * need to make bin directory in github build
    
    * adding additional echo
    
    * adding permissions
    
    * hardocoding roleid
    
    * permission change

--- build/managedapp/build-ma.sh
+++ build/managedapp/build-ma.sh
@@ -7,12 +7,19 @@ if [ $# -ne 1 ]; then
 fi
 
 MANAGED_APP_VERSION="$1"
+echo "The current directory is: $(pwd)"
 
-mkdir -p ../../obj
+mkdir -p ./obj
+mkdir -p ./bin
 
 DEPLOYED_IMAGE_REFERENCE="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$GALLERY_RESOURCE_GROUP/providers/Microsoft.Compute/galleries/$GALLERY_NAME/images/$GALLERY_IMAGE_DEFINITION/versions/$GALLERY_IMAGE_VERSION"
-UIDEF_FILE="createUiDefinition.json"
-TEMP_FILE="../../obj/createUiDefinition.json"
+UIDEF_FILE="./build/managedapp/createUiDefinition.json"
+TEMP_FILE="./obj/createUiDefinition.json"
+
+# Assign the Reader role to the Managed Application Service Principal
+# az role assignment create --assignee 1cf33839-e2dd-49a4-a41f-03a52b70a203 --role acdd72a7-3385-48ef-bd42-f606fba81ae7 --scope "$DEPLOYED_IMAGE_REFERENCE"
+# Assign the Reader role to the Managed Application Service Principal
+az role assignment create --assignee c3551f1c-671e-4495-b9aa-8d4adcd62976 --role acdd72a7-3385-48ef-bd42-f606fba81ae7 --scope "$DEPLOYED_IMAGE_REFERENCE"
 
 # Use sed to replace <IMAGE_REFERENCE> with the DEPLOYED_IMAGE_REFERENCE
 # The -i option has issues with certain platform implementations of the sed command,
@@ -20,20 +27,23 @@ TEMP_FILE="../../obj/createUiDefinition.json"
 rm $TEMP_FILE 2> /dev/null
 sed "s|<IMAGE_REFERENCE>|$DEPLOYED_IMAGE_REFERENCE|g" "$UIDEF_FILE" > "$TEMP_FILE"
 
-rm ../../obj/mainTemplate.json 2> /dev/null
-cp -f mainTemplate.json ../../obj/mainTemplate.json
+rm ./obj/mainTemplate.json 2> /dev/null
+cp -f ./build/managedapp/mainTemplate.json ./obj/mainTemplate.json
+cp -f ./build/managedapp/content.zip ./obj/content.zip
+
+echo "The ./obj directory contains: $(ls -la ./obj)"
 
 # Zip up the package for the managed application
-./createAppZip.sh
+./build/managedapp/createAppZip.sh
 
 
 # Create the Service Definition
 STORAGE_ACC_RESOURCE_GROUP=$MANAGED_APP_STORAGE_RG
 STORAGE_ACC_NAME=$MANAGED_APP_STORAGE_NAME
-STORAGE_CONTAINER_NAME="modm$MANAGED_APP_VERSION"
+BUILDNUM=$(echo "$MANAGED_APP_VERSION" | awk -F. '{print $3}')
+STORAGE_CONTAINER_NAME="modm$BUILDNUM"
 MA_RESOURCE_GROUP=$STORAGE_ACC_RESOURCE_GROUP
 MA_DEFINITION_NAME="modm$MANAGED_APP_VERSION"
 
-./createServiceDefinition.sh $STORAGE_ACC_RESOURCE_GROUP $STORAGE_ACC_NAME $STORAGE_CONTAINER_NAME $MA_RESOURCE_GROUP $MA_DEFINITION_NAME
+./build/managedapp/createServiceDefinition.sh $STORAGE_ACC_RESOURCE_GROUP $STORAGE_ACC_NAME $STORAGE_CONTAINER_NAME $MA_RESOURCE_GROUP $MA_DEFINITION_NAME
 
-az role assignment create --assignee c3551f1c-671e-4495-b9aa-8d4adcd62976 --role acdd72a7-3385-48ef-bd42-f606fba81ae7 --scope "$DEPLOYED_IMAGE_REFERENCE"
