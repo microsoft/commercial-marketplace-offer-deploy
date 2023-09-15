941059e50a374fe529014d3fcbe084e1b1331b3d
Bob Jacobs <bob.jacobs@outlook.com>
2023-09-15 13:59:09 -0400

    updating ArtifactsWatcherService (#434)
    * updating ArtifactsWatcherService
    
    * fixing type

---
+++ src/Core/Azure/Model/UserData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+namespace Modm.Azure.Model
+{
+    public class UserData
+    {
+        public required string ArtifactsUri { get; set; }
+
+        public string ToBase64Json()
+        {
+            string jsonString = JsonConvert.SerializeObject(this);
+            byte[] jsonBytes = Encoding.UTF8.GetBytes(jsonString);
+
+            return Convert.ToBase64String(jsonBytes);
+        }
+
+        public bool IsValid()
+        {
+            return !string.IsNullOrEmpty(this.ArtifactsUri)
+                && Uri.IsWellFormedUriString(this.ArtifactsUri, UriKind.Absolute);
+        }
+    }
+}
+
