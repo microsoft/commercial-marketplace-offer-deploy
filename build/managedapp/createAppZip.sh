0c16d137d089aed6eecaac4c6867b776609ad9bd
Bob Jacobs <bob.jacobs@outlook.com>
2023-09-15 23:17:31 -0400

    fixes to createAppZip.sh

--- build/managedapp/createAppZip.sh
+++ build/managedapp/createAppZip.sh
@@ -12,15 +12,19 @@ main_template_file="./obj/mainTemplate.json"
 create_ui_definition_file="./obj/createUiDefinition.json"
 
 # Change the directory to the scenario's location
+echo "Changing directory to ./build/managedapp/$scenario_name"
 cd "./build/managedapp/$scenario_name" || exit
+echo "The current directory is: $(pwd)"
 
 # Create the content.zip file
-zip -r "../../obj/content.zip" ./*
+zip -r "../../../obj/content.zip" ./*
 echo "zipped content.zip"
-echo "The ./obj directory contains: $(ls -la ./obj)"
 
 # Go back to the original directory
+echo "going back to origional directory"
 cd -
+echo "The current directory is: $(pwd)"
+echo "The ./obj directory contains: $(ls -la ./obj)"
 
 # Create the zip file including the specified files and directories
 zip -FS -j "$package_file" "$main_template_file" "$create_ui_definition_file" ./obj/content.zip
