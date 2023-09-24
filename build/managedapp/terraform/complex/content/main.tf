77fc54bbe687d6ef9aa61fd10566ce9b625a70d5
Bob Jacobs <bob.jacobs@outlook.com>
2023-09-24 16:11:07 -0400

    fixing app service block

--- build/managedapp/terraform/complex/content/main.tf
+++ build/managedapp/terraform/complex/content/main.tf
@@ -148,11 +148,9 @@ resource "azurerm_service_plan" "example_asp" {
   name                = local.app_service_plan_name
   location            = var.location
   resource_group_name = var.resource_group_name
-
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  os_type = "Linux"
+  sku_name = "F1"
+  sku_tier = "Free"
 }
 
 resource "azurerm_app_service" "example_app_service" {
