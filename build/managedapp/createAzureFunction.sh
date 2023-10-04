e8d8f43534ddb9cfaee162c3f142a43bed3e7d34
Bob Jacobs <bob.jacobs@outlook.com>
2023-10-04 19:50:34 -0400

    debugging

--- build/managedapp/createAzureFunction.sh
+++ build/managedapp/createAzureFunction.sh
@@ -5,6 +5,8 @@ if [ $# -ne 2 ]; then
   exit 1
 fi
 
+echo "inside createAzureFunction.sh"
+
 STORAGE_ACC_RESOURCE_GROUP="$1"
 STORAGE_ACC_NAME="$2"
 STORAGE_CONTAINER_NAME="$3"
@@ -13,13 +15,17 @@ original_dir=$(pwd)
 
 cd "./src/Functions" || exit
 
+echo "The current directory is: $(pwd). Publishing the Azure Function."
 # Publish the Azure Function
 dotnet publish -c Release -o ./publish
 
+echo "ls -la ./publish: $(ls -la ./publish)"
+
 # Zip the output
 cd publish
+echo "zipping the Azure Function."
 zip -r $original_dir/bin/functionapp.zip .
-
+echo "ls -la $original_dir/bin: $(ls -la $original_dir/bin)"
 
 # az storage account show --name "$STORAGE_ACC_NAME" --resource-group "$STORAGE_ACC_RESOURCE_GROUP" &>/dev/null
 # if [ $? -eq 0 ]; then
