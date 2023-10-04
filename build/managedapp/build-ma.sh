6a8407fc9e68fbecd45ba77b9ad7d5848a8d861f
Bob Jacobs <bob.jacobs@outlook.com>
2023-10-04 13:12:48 -0400

    echo update

--- build/managedapp/build-ma.sh
+++ build/managedapp/build-ma.sh
@@ -33,7 +33,7 @@ az role assignment create --assignee c3551f1c-671e-4495-b9aa-8d4adcd62976 --role
 rm $TEMP_FILE 2> /dev/null
 
 echo "prior to sed command - pwd is $(pwd)"
-echo "prior to sed command - ls of ./build/managedapp/terraform is $(ls -la ./build/managedapp/terraform)"
+echo "prior to sed command - ls of ./build/managedapp/terraform/vmoffer is $(ls -la ./build/managedapp/terraform/vmoffer)"
 echo "prior to sed comannd - the content of the ./build/managedapp/$SCENARIO directory is $(ls -la ./build/managedapp/$SCENARIO)"
 echo "prior to sed command, the UIDEF_FILE contains: $(cat $UIDEF_FILE)"
 
