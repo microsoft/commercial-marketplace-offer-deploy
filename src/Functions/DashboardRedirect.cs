050d0366489d8b658d4f13af506a4973f76d7b0f
Kevin Hillinger <kevin.hillinger@outlook.com>
2023-10-04 17:35:01 -0400

    Azure Function for Dashboard Redirect (#509)
    * adding function
    
    * updating
    
    * adding local settings
    
    ---------
    
    Co-authored-by: Bob Jacobs <bob.jacobs@outlook.com>

---
+++ src/Functions/DashboardRedirect.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Functions
+{
+    public class DashboardRedirect
+    {
+        private readonly ILogger _logger;
+
+        public DashboardRedirect(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<DashboardRedirect>();
+        }
+
+        [Function("DashboardRedirect")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
