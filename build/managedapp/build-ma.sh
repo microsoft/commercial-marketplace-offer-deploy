47cdacb1e81450a9ac7e437a355c9201e2d6d836
Bob Jacobs <bob.jacobs@outlook.com>
2023-10-04 19:33:02 -0400

    adding zipped azure functions

--- build/managedapp/build-ma.sh
+++ build/managedapp/build-ma.sh
@@ -19,13 +19,22 @@ echo "creating directories in $(pwd)"
 mkdir -p ./obj
 mkdir -p ./bin
 
-#DEPLOYED_IMAGE_REFERENCE="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$GALLERY_RESOURCE_GROUP/providers/Microsoft.Compute/galleries/$GALLERY_NAME/images/$GALLERY_IMAGE_DEFINITION/versions/$GALLERY_IMAGE_VERSION"
 DEPLOYED_IMAGE_REFERENCE="$2"
 echo "The deployed image reference is: $DEPLOYED_IMAGE_REFERENCE"
 UIDEF_FILE="./build/managedapp/$SCENARIO/createUiDefinition.json"
 TEMP_FILE="./obj/createUiDefinition.json"
 
 echo "The UIDEF_FILE is: $UIDEF_FILE"
+
+# Service Definition Vars
+STORAGE_ACC_RESOURCE_GROUP=$MANAGED_APP_STORAGE_RG
+STORAGE_ACC_NAME=$MANAGED_APP_STORAGE_NAME
+BUILDNUM=$(echo "$MANAGED_APP_VERSION" | awk -F. '{print $3}')
+MA_RESOURCE_GROUP=$STORAGE_ACC_RESOURCE_GROUP
+MODIFIED_SCENARIO=${SCENARIO//\//-}
+MA_DEFINITION_NAME="modm-$MODIFIED_SCENARIO-$MANAGED_APP_VERSION"
+MODIFIED_VERSION=${MANAGED_APP_VERSION//./-}
+STORAGE_CONTAINER_NAME="modm$MODIFIED_SCENARIO-$MODIFIED_VERSION"
 
 # Assign the Reader role to the Managed Application Service Principal
 az role assignment create --assignee c3551f1c-671e-4495-b9aa-8d4adcd62976 --role acdd72a7-3385-48ef-bd42-f606fba81ae7 --scope "$DEPLOYED_IMAGE_REFERENCE"
@@ -37,7 +46,10 @@ rm $TEMP_FILE 2> /dev/null
 
 echo "prior to sed command, the UIDEF_FILE contains: $(cat $UIDEF_FILE)"
 
-sed "s|<IMAGE_REFERENCE>|$DEPLOYED_IMAGE_REFERENCE|g" "$UIDEF_FILE" > "$TEMP_FILE"
+./createAzureFunctions.sh $STORAGE_ACC_RESOURCE_GROUP $STORAGE_ACC_NAME $STORAGE_CONTAINER_NAME
+$FUNCTION_BLOB=$(az storage blob url --account-name "$STORAGE_ACC_NAME" --container-name "$STORAGE_CONTAINER_NAME" --name functionapp.zip --output tsv)
+
+sed -e "s|<IMAGE_REFERENCE>|$DEPLOYED_IMAGE_REFERENCE|g" -e "s|<ZIPPED_FUNCTION>|$FUNCTION_BLOB|g" "$UIDEF_FILE" > "$TEMP_FILE"
 
 rm ./obj/mainTemplate.json 2> /dev/null
 rm ./obj/viewDefinition.json 2> /dev/null
@@ -50,16 +62,6 @@ echo "The ./obj directory contains: $(ls -la ./obj)"
 
 # Zip up the package for the managed application
 ./build/managedapp/createAppZip.sh $SCENARIO
-
-# Create the Service Definition
-STORAGE_ACC_RESOURCE_GROUP=$MANAGED_APP_STORAGE_RG
-STORAGE_ACC_NAME=$MANAGED_APP_STORAGE_NAME
-BUILDNUM=$(echo "$MANAGED_APP_VERSION" | awk -F. '{print $3}')
-MA_RESOURCE_GROUP=$STORAGE_ACC_RESOURCE_GROUP
-MODIFIED_SCENARIO=${SCENARIO//\//-}
-MA_DEFINITION_NAME="modm-$MODIFIED_SCENARIO-$MANAGED_APP_VERSION"
-MODIFIED_VERSION=${MANAGED_APP_VERSION//./-}
-STORAGE_CONTAINER_NAME="modm$MODIFIED_SCENARIO-$MODIFIED_VERSION"
 
 ./build/managedapp/createServiceDefinition.sh $STORAGE_ACC_RESOURCE_GROUP $STORAGE_ACC_NAME $STORAGE_CONTAINER_NAME $MA_RESOURCE_GROUP $MA_DEFINITION_NAME
 
