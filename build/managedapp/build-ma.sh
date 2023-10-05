9de4dc7e4de7f98a9d796784ded4383cfe43b432
Bob Jacobs <bob.jacobs@outlook.com>
2023-10-04 22:32:09 -0400

    adding function app

--- build/managedapp/build-ma.sh
+++ build/managedapp/build-ma.sh
@@ -22,7 +22,9 @@ mkdir -p ./bin
 DEPLOYED_IMAGE_REFERENCE="$2"
 echo "The deployed image reference is: $DEPLOYED_IMAGE_REFERENCE"
 UIDEF_FILE="./build/managedapp/$SCENARIO/createUiDefinition.json"
+VIEWDEF_FILE="./build/managedapp/$SCENARIO/viewDefinition.json"
 TEMP_FILE="./obj/createUiDefinition.json"
+TEMP_VIEWDEF_FILE="./obj/viewDefinition.json" 
 
 echo "The UIDEF_FILE is: $UIDEF_FILE"
 
@@ -35,6 +37,8 @@ MODIFIED_SCENARIO=${SCENARIO//\//-}
 MA_DEFINITION_NAME="modm-$MODIFIED_SCENARIO-$MANAGED_APP_VERSION"
 MODIFIED_VERSION=${MANAGED_APP_VERSION//./-}
 STORAGE_CONTAINER_NAME="modm$MODIFIED_SCENARIO-$MODIFIED_VERSION"
+FUNCTION_NAME=$STORAGE_CONTAINER_NAME
+FUNCTION_URL="https://$FUNCTION_NAME.azurewebsites.net"
 
 # Assign the Reader role to the Managed Application Service Principal
 az role assignment create --assignee c3551f1c-671e-4495-b9aa-8d4adcd62976 --role acdd72a7-3385-48ef-bd42-f606fba81ae7 --scope "$DEPLOYED_IMAGE_REFERENCE"
@@ -50,6 +54,7 @@ echo "prior to sed command, the UIDEF_FILE contains: $(cat $UIDEF_FILE)"
 FUNCTION_BLOB=$(az storage blob url --account-name "$STORAGE_ACC_NAME" --container-name "$STORAGE_CONTAINER_NAME" --name functionapp.zip --output tsv)
 
 sed -e "s|<IMAGE_REFERENCE>|$DEPLOYED_IMAGE_REFERENCE|g" -e "s|<ZIPPED_FUNCTION>|$FUNCTION_BLOB|g" "$UIDEF_FILE" > "$TEMP_FILE"
+sed -e "s|<FUNCTION_URL>|$FUNCTION_URL|g" "$VIEWDEF_FILE" > "$TEMP_VIEWDEF_FILE"
 
 rm ./obj/mainTemplate.json 2> /dev/null
 rm ./obj/viewDefinition.json 2> /dev/null
