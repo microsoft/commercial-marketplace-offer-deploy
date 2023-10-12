e5538a76d537ef56c175600fc98635e0fe1ba193
Bob Jacobs <bob.jacobs@outlook.com>
2023-10-12 11:22:03 -0400

    removing live function test

--- test/Tests/UnitTests/FunctionPublisherTests.cs
+++ test/Tests/UnitTests/FunctionPublisherTests.cs
@@ -13,29 +13,29 @@ namespace Modm.Tests.UnitTests
 		{
 		}
 
-        [Fact]
-        public async Task Azure_Function_Should_Deploy()
-		{
-			string functionAppName = "bobjactemplate";
+  //      [Fact]
+  //      public async Task Azure_Function_Should_Deploy()
+		//{
+		//	string functionAppName = "bobjactemplate";
 
-            // Determine the location of the currently executing assembly.
-            var currentAssemblyLocation = new Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath;
+  //          // Determine the location of the currently executing assembly.
+  //          var currentAssemblyLocation = new Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath;
 
-            // Determine the directory from that location.
-            var currentDirectory = Path.GetDirectoryName(currentAssemblyLocation);
+  //          // Determine the directory from that location.
+  //          var currentDirectory = Path.GetDirectoryName(currentAssemblyLocation);
 
-            // Construct the relative path to the Azure Function directory.
-            var relativeFunctionPath = "../../../../../src/Functions";
-            var workingDirectory = Path.GetFullPath(Path.Combine(currentDirectory, relativeFunctionPath));
+  //          // Construct the relative path to the Azure Function directory.
+  //          var relativeFunctionPath = "../../../../../src/Functions";
+  //          var workingDirectory = Path.GetFullPath(Path.Combine(currentDirectory, relativeFunctionPath));
 
-            if (!Directory.Exists(workingDirectory))
-            {
-                Assert.Fail("Working Directory Doesn't exist");
-            }
+  //          if (!Directory.Exists(workingDirectory))
+  //          {
+  //              Assert.Fail("Working Directory Doesn't exist");
+  //          }
 
-            var publisher = new FunctionPublisher(workingDirectory);
-			await publisher.PublishAsync(functionAppName);
-        }
+  //          var publisher = new FunctionPublisher(workingDirectory);
+		//	await publisher.PublishAsync(functionAppName);
+  //      }
     }
 }
 
