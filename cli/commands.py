adf1b77c9c817143c703935701a6e41b2546385e
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-10-12 14:35:27 -0400

    update

--- cli/commands.py
+++ cli/commands.py
@@ -1,8 +1,8 @@
 import click
 
 @click.command()
-@click.option("-t", "--main-template", default="", help="The application's main template.")
-@click.option("--name", prompt="Your name", help="The person to greet.")
+@click.option("-t", "--main-template", default="", help="The path to the application's main template.")
+@click.option("-u", "--create-ui-definition", prompt="Your name", help="The path to the createUiDefinition.json file")
 def build(count, name):
     """Builds an application package, e.g. an app.zip"""
     for _ in range(count):
