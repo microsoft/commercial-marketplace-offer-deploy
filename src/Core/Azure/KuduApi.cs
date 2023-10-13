9db116db28ff9e8ba12e4361e0542ea75f4fb414
Bob Jacobs <bob.jacobs@outlook.com>
2023-10-13 01:17:53 -0400

    adding kuduapi

---
+++ src/Core/Azure/KuduApi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Azure.Identity;
+using Azure.Core;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Modm.Azure
+{
+    public class KuduApi
+    {
+        private readonly HttpClient client;
+        private readonly string functionAppName;
+        private readonly DefaultAzureCredential credential;
+
+        public KuduApi(string functionAppName, HttpClient client)
+        {
+            this.functionAppName = functionAppName;
+            this.client = client;
+            this.credential = new DefaultAzureCredential();
+        }
+
+        public async Task DeployZipAsync(string zipFilePath)
+        {
+            var baseUrl = $"https://{functionAppName}.scm.azurewebsites.net/api/zipdeploy/";
+
+            using var request = new HttpRequestMessage(HttpMethod.Post, baseUrl);
+
+            var token = await credential.GetTokenAsync(new TokenRequestContext(new[] { "https://management.azure.com/.default" }));
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token.Token);
+
+            using var fileStream = new FileStream(zipFilePath, FileMode.Open);
+            using var streamContent = new StreamContent(fileStream);
+
+            request.Content = streamContent;
+
+            var response = await this.client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
+
---
+++ test/Tests/UnitTests/KuduApiTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Modm.Azure;
+namespace Modm.Tests.UnitTests
+{
+	public class KuduApiTests
+	{
+		public KuduApiTests()
+		{
+		}
+
+        [Fact]
+		public async Task should_deploy_zip()
+		{
+            string functionAppName = "bobjacazinsider3";
+
+            var currentAssemblyLocation = new Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath;
+
+            // Determine the directory from that location.
+            var currentDirectory = Path.GetDirectoryName(currentAssemblyLocation);
+
+            // Construct the relative path to the Azure Function directory.
+            var relativeFunctionPath = "../../../Data";
+            var dataDirectory = Path.GetFullPath(Path.Combine(currentDirectory, relativeFunctionPath));
+            
+
+            if (!Directory.Exists(dataDirectory))
+            {
+                Assert.Fail("Working Directory Doesn't exist");
+            }
+
+            var zipFilePath = Path.Combine(dataDirectory, "azurefunction.zip");
+            var kuduApi = new KuduApi(functionAppName, new HttpClient());
+            await kuduApi.DeployZipAsync(zipFilePath);
+            
+        }
+    }
+}
+
