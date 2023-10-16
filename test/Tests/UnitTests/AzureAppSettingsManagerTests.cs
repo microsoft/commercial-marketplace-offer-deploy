06786742328f86d23fe0529287cbed7ab10ea3d3
Bob Jacobs <bob.jacobs@outlook.com>
2023-10-16 13:15:20 -0400

    removing test

--- test/Tests/UnitTests/AzureAppSettingsManagerTests.cs
+++ test/Tests/UnitTests/AzureAppSettingsManagerTests.cs
@@ -9,21 +9,21 @@ namespace Modm.Tests.UnitTests
 		{
 		}
 
-        [Fact]
-        public async Task appsettings_should_update()
-		{
-            string functionAppName = "bobjacazinsider3";
+  //      [Fact]
+  //      public async Task appsettings_should_update()
+		//{
+  //          string functionAppName = "bobjacazinsider3";
 
-            var resourceGroupName = "bobjacfunctions";
-            var subscriptionId = "<SUBSCRIPTIONID>";
+  //          var resourceGroupName = "bobjacfunctions";
+  //          var subscriptionId = "<SUBSCRIPTIONID>";
 
-            var appSettingsManager = new AzureAppSettingsManager(subscriptionId);
-            var redirectUrl = $"http://www.updated.com";
-            await appSettingsManager.UpdateAppSettingsAsync(resourceGroupName, functionAppName, new Dictionary<string, string>
-                {
-                    { "RedirectUrl", redirectUrl }
-                });
-        }
+  //          var appSettingsManager = new AzureAppSettingsManager(subscriptionId);
+  //          var redirectUrl = $"http://www.updated.com";
+  //          await appSettingsManager.UpdateAppSettingsAsync(resourceGroupName, functionAppName, new Dictionary<string, string>
+  //              {
+  //                  { "RedirectUrl", redirectUrl }
+  //              });
+  //      }
     }
 }
 
