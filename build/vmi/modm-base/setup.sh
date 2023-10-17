e898d9b9a72627c26312d61099af74ba2c313fac
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-10-17 07:59:47 -0400

    clean up base setup script

--- build/vmi/modm-base/setup.sh
+++ build/vmi/modm-base/setup.sh
@@ -17,6 +17,8 @@ echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selecti
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
+sudo apt-get install jq -y
+
 # Docker engine
 sudo apt-get install ca-certificates curl gnupg -y
 
@@ -39,18 +41,6 @@ sudo apt-get install docker-buildx-plugin docker-compose -y
 
 # Install .NET 7
 sudo apt-get install -y dotnet-sdk-7.0
-
-echo "Installing Azure CLI"
-
-# Install Azure Functions Core needed for the Azure Function App
-# This will be used to publish the dashboard redirect
-#curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-#sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-
-#sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
-
-#sudo apt-get update
-#sudo apt-get install azure-functions-core-tools-4
 
 # clone the MODM source into source
 sudo git clone --depth=1 --branch $MODM_REPO_BRANCH https://github.com/microsoft/commercial-marketplace-offer-deploy.git $MODM_HOME/source
