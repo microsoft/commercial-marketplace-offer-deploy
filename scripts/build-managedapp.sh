60e17f62972f6fb3c16bf26fbaf70869ae91228c
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-10-25 14:50:08 -0400

    updating script ex

--- scripts/build-managedapp.sh
+++ scripts/build-managedapp.sh
@@ -10,11 +10,11 @@
 #     Calls the build with required arguments, using default env vars at ./obj/.env.pkrvars
 #     when called locally
 #
-#       ./scripts/build-managedapp.sh -v 0.0.301 \
+#       ./scripts/build-managedapp.sh \
 #           --scenario terraform/simple \
-#           --version 0.1.20 \
+#           --version 0.1.134 \
 #           --resource-group modm-dev \
-#           --image-id /subscriptions/.../images/modm/versions/0.0.222 \
+#           --image-id /subscriptions/.../images/modm/versions/0.1.134 \
 #           --storage-account modmsvccatalog
 # ===========================================================================================
 
