0a79e5292b7e60894e0284feaacfafea152acfd7
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-10-26 10:52:26 -0400

    updating to only grab the api token for jenkins once

--- src/Core/Jenkins/Client/JenkinsClientFactory.cs
+++ src/Core/Jenkins/Client/JenkinsClientFactory.cs
@@ -11,6 +11,11 @@ namespace Modm.Jenkins.Client
         private readonly ApiTokenClient apiTokenClient;
         private readonly IServiceProvider serviceProvider;
 
+        private readonly Lazy<Task<string>> apiToken;
+
+        /// <summary>
+        /// Constructor without params only to support testing
+        /// </summary>
         public JenkinsClientFactory()
         {
         }
@@ -21,12 +26,12 @@ namespace Modm.Jenkins.Client
             this.httpClient = client;
             this.apiTokenClient = apiTokenClient;
             this.serviceProvider = serviceProvider;
+            this.apiToken = new(apiTokenClient.Get, LazyThreadSafetyMode.PublicationOnly);
         }
 
         public virtual async Task<IJenkinsClient> Create()
         {
-            // to start making calls to Jenkins, an API Token is required. Fetch this token using the provider
-            var apiToken = await apiTokenClient.Get();
+            var apiToken = await GetApiToken();
             var jenkinsNetClient = new JenkinsNET.JenkinsClient(options.BaseUrl)
             {
                 UserName = options.UserName,
@@ -39,6 +44,16 @@ namespace Modm.Jenkins.Client
             var client = new JenkinsClient(httpClient, jenkinsNetClient, logger, options);
 
             return client;
+        }
+
+        /// <summary>
+        /// to start making calls to Jenkins, an API Token is required. Fetch this token using the provider
+        /// </summary>
+        /// <returns></returns>
+        public async ValueTask<string> GetApiToken()
+        {
+            var value = await apiToken.Value.ConfigureAwait(false);
+            return value;
         }
     }
 }
