c7af3974d653bfd2fe9852efb7c7d598802ea65c
Bob Jacobs <bob.jacobs@outlook.com>
2023-10-25 20:10:36 -0400

    updating EngineChecker

--- src/ServiceHost/EngineChecker.cs
+++ src/ServiceHost/EngineChecker.cs
@@ -8,11 +8,13 @@ public class EngineChecker
 {
     private readonly string BaseUrl;
     private readonly HttpClient httpClient;
+    ILogger<EngineChecker> logger;
 
-    public EngineChecker(string baseUrl, HttpClient httpClient)
+    public EngineChecker(string baseUrl, HttpClient httpClient, ILogger<EngineChecker> logger)
     {
         this.BaseUrl = baseUrl;
         this.httpClient = httpClient;
+        this.logger = logger;
     }
   
     public async Task<bool> IsEngineHealthy()
@@ -22,11 +24,19 @@ public class EngineChecker
             var response = await httpClient.GetAsync($"{this.BaseUrl}/api/status");
             if (!response.IsSuccessStatusCode)
             {
-                throw new Exception($"HTTP error! status: {response.StatusCode}");
+                this.logger.LogError($"Engine is not healthy. Status code: {response.StatusCode}");
+                return false;
             }
 
             var jsonResponse = await response.Content.ReadAsStringAsync();
+            this.logger.LogInformation($"Engine status: {jsonResponse}");
             var statusData = JsonSerializer.Deserialize<StatusData>(jsonResponse);
+            if (statusData == null)
+            {
+                this.logger.LogError($"Engine is not healthy. Status data is null.");
+                return false;
+            }
+            this.logger.LogInformation($"Engine status after deserialization: {statusData.IsHealthy}");
             if (statusData == null)
             {
                 return false;
