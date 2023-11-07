5afbd6e45883e1f4e79fd47277334b127ba2783b
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-11-07 16:29:46 -0500

    updating appconfig wireup

--- src/WebHost/Program.cs
+++ src/WebHost/Program.cs
@@ -13,14 +13,17 @@ builder.Services.AddCors(options =>
     });
 });
 
-var appConfigEndpoint = builder.Configuration["Azure:AppConfigEndpoint"] ?? string.Empty;
-
-if (!string.IsNullOrEmpty(appConfigEndpoint))
+if (!builder.Environment.IsDevelopment())
 {
-    builder.Configuration.AddAzureAppConfiguration(options =>
-      options.Connect(
-          new Uri(appConfigEndpoint),
-          new DefaultAzureCredential()));
+    var appConfigEndpoint = builder.Configuration["Azure:AppConfigEndpoint"] ?? string.Empty;
+
+    if (!string.IsNullOrEmpty(appConfigEndpoint))
+    {
+        builder.Configuration.AddAzureAppConfiguration(options =>
+          options.Connect(
+              new Uri(appConfigEndpoint),
+              new ManagedIdentityCredential()));
+    }
 }
 
 var app = builder.Build();
