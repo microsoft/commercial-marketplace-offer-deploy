adf6426baf87a3baae3b79391959e7b3366ced9c
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-11-08 11:14:14 -0500

    fix failing tests

--- tests/UnitTests/DirectoryZipperTests.cs
+++ tests/UnitTests/DirectoryZipperTests.cs
@@ -1,48 +1,48 @@
-﻿using System;
-using System.IO;
-using System.IO.Compression;
+﻿using System.IO.Compression;
 using Modm.Compression;
-using Xunit;
 
-public class DirectoryZipperTests
+namespace Modm.Tests.UnitTests
 {
-    [Fact]
-    public void ZipDirectory_CreatesZipOfDirectory()
+    public class DirectoryZipperTests
     {
-        // Arrange
-        var sourceDirectory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-        var destinationZip = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".zip");
-        Directory.CreateDirectory(sourceDirectory);
-        File.WriteAllText(Path.Combine(sourceDirectory, "test.txt"), "Hello, World!");
+        [Fact]
+        public void ZipDirectory_CreatesZipOfDirectory()
+        {
+            // Arrange
+            var sourceDirectory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            var destinationZip = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".zip");
+            Directory.CreateDirectory(sourceDirectory);
+            File.WriteAllText(Path.Combine(sourceDirectory, "test.txt"), "Hello, World!");
 
-        var zipper = new DirectoryZipper();
+            var zipper = new DirectoryZipper();
 
-        // Act
-        zipper.ZipDirectory(sourceDirectory, destinationZip);
+            // Act
+            zipper.ZipDirectory(sourceDirectory, destinationZip);
 
-        // Assert
-        Assert.True(File.Exists(destinationZip));
+            // Assert
+            Assert.True(File.Exists(destinationZip));
 
-        // Check if the file inside the zip is as expected
-        using var archive = ZipFile.OpenRead(destinationZip);
-        var entry = archive.GetEntry("test.txt");
-        Assert.NotNull(entry);
+            // Check if the file inside the zip is as expected
+            using var archive = ZipFile.OpenRead(destinationZip);
+            var entry = archive.GetEntry("test.txt");
+            Assert.NotNull(entry);
 
-        // Cleanup
-        Directory.Delete(sourceDirectory, true);
-        File.Delete(destinationZip);
-    }
+            // Cleanup
+            Directory.Delete(sourceDirectory, true);
+            File.Delete(destinationZip);
+        }
 
-    [Fact]
-    public void ZipDirectory_ThrowsWhenSourceDirectoryDoesNotExist()
-    {
-        // Arrange
-        var nonExistentDirectory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-        var destinationZip = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".zip");
+        [Fact]
+        public void ZipDirectory_ThrowsWhenSourceDirectoryDoesNotExist()
+        {
+            // Arrange
+            var nonExistentDirectory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            var destinationZip = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".zip");
 
-        var zipper = new DirectoryZipper();
+            var zipper = new DirectoryZipper();
 
-        // Act & Assert
-        Assert.Throws<DirectoryNotFoundException>(() => zipper.ZipDirectory(nonExistentDirectory, destinationZip));
+            // Act & Assert
+            Assert.Throws<DirectoryNotFoundException>(() => zipper.ZipDirectory(nonExistentDirectory, destinationZip));
+        }
     }
 }
