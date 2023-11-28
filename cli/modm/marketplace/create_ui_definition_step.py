14d5d80f60c5956f8beb3ea4c7e7119a78f15358
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-11-28 13:39:33 -0500

    fixing create ui definition template and output paths not matching

--- cli/modm/marketplace/create_ui_definition_step.py
+++ cli/modm/marketplace/create_ui_definition_step.py
@@ -5,9 +5,8 @@ from modm.marketplace.create_ui_definition import CreateUiDefinition
 
 class CreateUiDefinitionInstallerStep:
     name = "installer"
-    output_value_format = "[steps('{}')]"
 
-    def __init__(self, step: dict):
+    def __init__(self, step: dict, outputs: dict):
             """
             Initializes a new instance of the CreateUiDefinitionStep class.
 
@@ -15,17 +14,15 @@ class CreateUiDefinitionInstallerStep:
                 document (dict): The UI definition step
             """
             self.step = step
+            self.outputs = outputs
 
     def append_to(self, create_ui_definition: CreateUiDefinition):
         document = create_ui_definition.document
         steps = document["parameters"]["steps"]
         steps.append(self.step)
 
-        outputs = document["parameters"]["outputs"]
-
-        # Add the output value for the installer step
-        outputs["_installerUsername"] = "[steps('installer').username]"
-        outputs["_installerPassword"] = "[steps('installer').password]"
+        outputs: dict = document["parameters"]["outputs"]
+        outputs.update(self.outputs)
 
 
 def from_file(file_path) -> CreateUiDefinitionInstallerStep:
@@ -35,4 +32,5 @@ def from_file(file_path) -> CreateUiDefinitionInstallerStep:
     with open(file_path, "r") as f:
         document = json.load(f)
         step = document["parameters"]["steps"][0]
-        return CreateUiDefinitionInstallerStep(step)
+        outputs = document["parameters"]["outputs"]
+        return CreateUiDefinitionInstallerStep(step, outputs)
