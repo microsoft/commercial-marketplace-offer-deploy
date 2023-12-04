3cd6fa4f65e1ae201483245e4bad34a787f4f93d
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-12-04 06:47:19 -0500

    release manager updates

--- cli/modm/release/_release_manager.py
+++ cli/modm/release/_release_manager.py
@@ -1,38 +1,144 @@
-
 from pathlib import Path
 import tempfile
+from modm.installer.client_app_package import ClientAppPackage
 from modm.release.version import Version
 from .release_info import ReleaseInfo
 from ghapi.all import GhApi
-from fastcore.net import HTTP4xxClientError
 import os
 import shutil
 import warnings
+import subprocess
+from typing import Callable
+from modm import _zip_utils
 
 class ReleaseManager:
     """
     Uses: https://ghapi.fast.ai/fullapi.html
     """
+
     owner = "microsoft"
     repo = "commercial-marketplace-offer-deploy"
 
-    def __init__(self):
-        self.api: GhApi = self._get_api_client()
+    def __init__(self, github_auth_token: str = None, out: Callable[[str], None] = None):
+        self.out = out if out is not None else lambda *args: None
+        self.api: GhApi = self._get_api_client(github_auth_token)
         self.cwd = os.getcwd()
 
-    def _get_api_client(self):
-        """Returns a GitHub API client"""
+    def create(self, release_info: ReleaseInfo):
+        version = Version(release_info.version)
+
+        if self._release_exists(version):
+            raise ValueError(f"Release {version.name} already exists")
+
+        self._create_tag(version)
+        clone_dir = self._create_shallow_clone(version)
+
+        resources_archive = self._create_resources_archive(clone_dir)
+
+        # 5. build the wheel of the python library for the version
+        wheel_file = "/path/to/wheel/file"
+        os.system("python setup.py bdist_wheel")
+
+        # 6. create a draft GH release for the tagged version
+        release = self.github.repos.create_release(tag_name=version.name, draft=True)
+
+        # 7. upload the files
+        #  - the wheel
+        self.github.repos.upload_release_asset(release_id=release.id, name="wheel", asset=wheel_file)
+        
+        #  - the resources archive
+        self.github.repos.upload_release_asset(release_id=release.id, name="resources", asset=resources_archive)
+
+    def _create_resources_archive(self, clone_dir: Path) -> Path:
+        cwd = Path(current_working_dir)
+        out_dir = _resolve_path(cwd, out_dir)
+
+        templates_dir = clone_dir / "templates"
+        main_template_file = templates_dir / "mainTemplate.json"
+        view_definition_file = templates_dir/ "viewDefinition.json"
+        create_ui_definition_file = templates_dir / "createUiDefinition.json"
+        client_app_package_file = self._create_client_app_package(clone_dir)
+
+        if version is not None:
+            installer_version = Version(version)
+            out_file = out_dir / f"resources-{installer_version.name}.tar.gz"
+        else:
+            out_file = out_dir / f"resources.tar.gz"
+
+        click.echo(f"Creating tarball {out_file}...")
+
+        with tarfile.open(out_file, "w:gz") as tar:
+            tar.add(main_template_file, arcname=main_template_file.name)
+            tar.add(view_definition_file, arcname=view_definition_file.name)
+            tar.add(create_ui_definition_file, arcname=create_ui_definition_file.name)
+            tar.add(client_app_file, arcname=client_app_file.name)
+
+        click.echo(f"resources '{out_file.name}' created.")
+
+        if version is not None:
+            result = {}
+            result[
+                "downloadUrl"
+            ] = f"https://github.com/microsoft/commercial-marketplace-offer-deploy/releases/download/{installer_version.name}/{out_file.name}"
+            result["filename"] = out_file.name
+            result["sha256Digest"] = _get_sha256_digest(out_file)
+        else:
+            result = {}
+            result["filename"] = str(out_file)
+            result["sha256Digest"] = _get_sha256_digest(out_file)
+
+        click.echo(json.dumps(result, indent=2))
+
+    def _create_client_app_package(self, clone_dir: Path) -> Path:
+        """creates a clientapp.zip"""
+        out_dir = clone_dir / "dist"
+
+        if not out_dir.exists():
+            out_dir.mkdir()
+
+        csproj_file = clone_dir / "src" / "ClientApp" / "ClientApp.csproj"
+
+        process = subprocess.Popen(["dotnet", "publish", csproj_file, "-c", "Release", "-o", out_dir], stdout=subprocess.PIPE, universal_newlines=True)
+
+        self.out("Building client app.")
+
+        while True:
+            output = process.stdout.readline()
+            if len(output) > 0:
+                self.out("  " + output.strip())
+            # Do something else
+            return_code = process.poll()
+            if return_code is not None:
+                # Process has finished, read rest of the output
+                for output in process.stdout.readlines():
+                    if len(output) > 0:
+                        self.out("  " + output.strip())
+                break
+
+        self.out("Creating client app package.")
+
+        out_file = out_dir / ClientAppPackage.file_name
+        _zip_utils.zip_dir(out_dir, out_file)
+
+        return out_file
+
+    def _get_api_client(self, github_auth_token: str = None):
+        """
+        Returns a GitHub API client
+        If github_auth_token is None, the client will be attempt to authenticate using the standard GitHub environment
+        variables (which is the default)
+        """
 
         # the GitHub API client only warns if authentication fails. We need to catch this and exit if it happens
         warnings.filterwarnings("error")
         api = None
 
         try:
-            api = GhApi(owner='microsoft', repo="commercial-marketplace-offer-deploy", authenticate=True)
+            api = GhApi(owner="microsoft", repo="commercial-marketplace-offer-deploy", token=github_auth_token, authenticate=True)
         except UserWarning as e:
             if "unauthenticated" in str(e):
                 err = RuntimeError("Authentication failed")
-        
+
         warnings.resetwarnings()
 
         # try to get something with the authenticated user. if it fails, raise an error
@@ -55,7 +161,7 @@ class ReleaseManager:
 
     def _create_tag(self, version: Version):
         """Creates a tag for the given version"""
-        main_ref = self.api.git.get_ref('heads/main')
+        main_ref = self.api.git.get_ref("heads/main")
         tag = self.api.git.create_tag(tag=version.name, message=version.name, object=main_ref.object.sha, type="commit")
         self.api.git.create_ref(ref=f"refs/tags/{version.name}", sha=tag.sha)
 
@@ -63,34 +169,7 @@ class ReleaseManager:
         """Creates a shallow clone of the given version"""
         temp_dir = tempfile.mkdtemp()
         os.chdir(temp_dir)
-        repo_url = f"https://github.com/microsoft/commercial-marketplace-offer-deploy.git"
+        repo_url = f"https://github.com/{ReleaseManager.owner}/commercial-marketplace-offer-deploy.git"
         os.system(f"git clone --depth 1 --branch {version.name} {repo_url} .")
 
         return Path(temp_dir)
-
-    def create(self, release_info: ReleaseInfo):
-        version = Version(release_info.version)
-        
-        if self._release_exists(version):
-            raise ValueError(f"Release {version.name} already exists")
-
-        self._create_tag(version)
-        clone_dir = self._create_shallow_clone(version)
-        
-        # 4. build the resources archive for the version
-        resources_archive = "/path/to/resources/archive"
-        shutil.make_archive(resources_archive, "zip", "/path/to/resources")
-
-        # 5. build the wheel of the python library for the version
-        wheel_file = "/path/to/wheel/file"
-        os.system("python setup.py bdist_wheel")
-
-        # 6. create a draft GH release for the tagged version 
-        release = self.github.repos.create_release(tag_name=version.name, draft=True)
-
-        # 7. upload the files
-        #  - the wheel 
-        self.github.repos.upload_release_asset(release_id=release.id, name="wheel", asset=wheel_file)
-        #  - the resources archive
-        self.github.repos.upload_release_asset(release_id=release.id, name="resources", asset=resources_archive)
-
