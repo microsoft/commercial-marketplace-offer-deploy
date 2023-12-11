3bdac8aad7728ea6d72cfe05083767f20a3016fd
Bob Jacobs <bob.jacobs@outlook.com>
2023-12-10 20:17:39 -0500

    fixing quote

--- build/managedapp/build.sh
+++ build/managedapp/build.sh
@@ -112,7 +112,8 @@ function createServiceDefinition() {
     echo "expiry: $expiry"
 
 
-    echo "Generating SAS token for app.zip." with Storage Account Connection String: $storage_account_connection_string, expiry: $expiry, storage account name: $STORAGE_ACCOUNT_NAME, storage container name: $STORAGE_CONTAINER_NAME, blob name: app.zip"
+    echo "Generating SAS token for app.zip." with Storage Account Connection String: $storage_account_connection_string expiry: $expiry storage account name: $STORAGE_ACCOUNT_NAME, storage container name: $STORAGE_CONTAINER_NAME"
+
     blob=$(az storage blob generate-sas \
         --account-name $STORAGE_ACCOUNT_NAME \
         --container-name $STORAGE_CONTAINER_NAME \
