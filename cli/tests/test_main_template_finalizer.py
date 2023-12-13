345cee976e4244c6f4055b276c6be1c8f6c226eb
Kevin Hillinger <kevin.hillinger@outlook.com>
2023-12-13 10:26:46 -0500

    fix: cli package build (#641)
    * fixing package build
    
    * fixing tests

--- cli/tests/test_main_template_finalizer.py
+++ cli/tests/test_main_template_finalizer.py
@@ -4,7 +4,7 @@ import unittest
 
 from modm.marketplace import MainTemplate, MainTemplateFinalizer
 from modm.installer.installer_package_result import InstallerPackageResult
-from modm.release.release_info import ReferenceInfo, OfferInfo
+from modm.release.release_info import ReleaseInfo
 from tests import TestCaseBase
 
 
@@ -12,15 +12,18 @@ class TestMainTemplateFinalizer(TestCaseBase):
     def setUp(self):
         self.main_template = MainTemplate.from_file(self.data_path / "mainTemplate.json")
 
-        self.reference_info: ReferenceInfo = ReferenceInfo.from_dict({
-            "vmi": "test_id", 
-            "offer": { 
-                "plan": { "name": "", "publisher": "", "product": ""}, 
-                "imageReference": {
-                    "publisher": "test_publisher",
-                    "offer": "test_offer",
-                    "sku": "test_sku",
-                    "version": "test_version"
+        self.release_info = ReleaseInfo.from_dict({
+            "version": "1.0.0",
+            "reference": {
+                "vmi": "test_id", 
+                "offer": { 
+                    "plan": { "name": "", "publisher": "", "product": ""}, 
+                    "imageReference": {
+                        "publisher": "test_publisher",
+                        "offer": "test_offer",
+                        "sku": "test_sku",
+                        "version": "test_version"
+                    }
                 }
             }
         })
@@ -32,7 +35,7 @@ class TestMainTemplateFinalizer(TestCaseBase):
 
     def test_finalize_with_false_use_vmi_reference(self):
         installer_package=InstallerPackageResult(self.data_path / "installer.zip")
-        result = self.finalizer.finalize(reference_info=self.reference_info, use_vmi_reference=False, installer_package=installer_package)
+        result = self.finalizer.finalize(release_info=self.release_info, use_vmi_reference=False, installer_package=installer_package)
 
-        self.assertEqual(result.vm_offer, self.reference_info.offer.serialize())
+        self.assertEqual(result.vm_offer, self.release_info.reference.offer.serialize())
 
