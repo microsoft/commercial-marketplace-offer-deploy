d997bb8891584dda41c4016332fc77ee0237dde3
Bob Jacobs <bob.jacobs@outlook.com>
2024-02-20 13:20:38 -0500

    documentation update

---
+++ docs/notebooks/complexterraform/modules/networking/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Azure region for the resources"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which resources will be deployed"
+  type        = string
+}
