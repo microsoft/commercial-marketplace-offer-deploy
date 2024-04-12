846ef43f7d2ead29e37aac02ada69a66b66c75b9
Bob Jacobs <bob.jacobs@outlook.com>
2024-04-12 14:33:20 -0400

    adding the method to retrieve parameters

--- src/WebHost/Controllers/DeploymentsController.cs
+++ src/WebHost/Controllers/DeploymentsController.cs
@@ -26,6 +26,45 @@ namespace WebHost.Controllers
             });
         }
 
+         /// <summary>
+        /// Gets the parameters associated with a deployment
+        /// </summary>
+        [HttpGet("{deploymentId}/parameters")]
+        public async Task<IActionResult> GetParametersFileContent(string deploymentId)
+        {
+            try
+            {
+                Deployment deployment = await engine.Get();
+
+                // Determine the path to the parameters file
+                string parametersFilePath = deployment?.Definition?.ParametersFilePath;
+
+                if (!System.IO.File.Exists(parametersFilePath))
+                {
+                    throw new FileNotFoundException();
+                }
+
+                string content = await System.IO.File.ReadAllTextAsync(parametersFilePath);
+
+                return new ContentResult
+                {
+                    ContentType = "application/json",
+                    Content = content,
+                    StatusCode = 200
+                };
+            }
+            catch (FileNotFoundException)
+            {
+                return NotFound("Parameters file not found.");
+            }
+            catch (Exception ex)
+            {
+                // Log the exception details
+                // Return a generic error message to the client
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
+        }
+
         /// <summary>
         /// Creates a deployment by submitting to the deployment engine
         /// </summary>
