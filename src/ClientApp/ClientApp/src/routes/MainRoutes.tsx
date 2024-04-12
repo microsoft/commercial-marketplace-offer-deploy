3d9c2960bff24cc5236ec489fba7333d7d7100d0
Bob Jacobs <bob.jacobs@outlook.com>
2024-04-12 14:40:45 -0400

    updating main routes to include redeploy

--- src/ClientApp/ClientApp/src/routes/MainRoutes.tsx
+++ src/ClientApp/ClientApp/src/routes/MainRoutes.tsx
@@ -5,6 +5,7 @@ import { RouteObject } from 'react-router-dom';
 import MainLayout from '../layout/MainLayout';
 import Default from '../views/Default';
 import Diagnostics from '../views/Diagnostics';
+import Redeploy from '@/views/Redeploy';
 import LoginPage from '../views/LoginPage';
 import ProtectedRoute from '../security/ProtectedRoute';
 
@@ -16,6 +17,10 @@ const MainRoutes: RouteObject[] = [
       {
         index: true,
         element: <ProtectedRoute component={Default} />,
+      },
+      {
+        path: 'redeploy',
+        element: <ProtectedRoute component={Redeploy} />,
       },
       {
         path: 'diagnostics',
